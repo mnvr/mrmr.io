3c76f508d44b08522cb6ef97a71277d281619dbe
Manav <manav@mrmr.io>
2023-03-04 03:21:43 +0530

    Change title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,20 @@ import type { HeadFC, PageProps } from "gatsby";
 import * as React from "react";
 import styled from "styled-components";
 
+const IndexPage: React.FC<PageProps> = () => {
+    return (
+        <Main>
+            <H1>mrmr</H1>
+            <Poem />
+            <Items />
+        </Main>
+    );
+};
+
+export default IndexPage;
+
+export const Head: HeadFC = () => <title>Home</title>;
+
 const items = [
     {
         text: "come dream with me",
@@ -9,6 +23,10 @@ const items = [
         color: "#3C1DFE",
     },
 ];
+
+const Main = styled.main`
+    margin: auto;
+`;
 
 const H1 = styled.h1`
     font-size: 4rem;
@@ -18,29 +36,40 @@ const H1 = styled.h1`
     color: hsl(0, 0%, 13.3%);
 `;
 
-const L = styled.span`
-    color: hsl(0, 0%, 53.3%);
-`;
-
-const Main = styled.main`
-    margin: auto;
-`;
-
-const IndexPage: React.FC<PageProps> = () => {
+const Items: React.FC = () => {
     return (
-        <Main>
-            <H1>
-                m<L>u</L>rm<L>u</L>r<L></L>
-            </H1>
-            <ul style={{ color: "white" }}>
-                {items.map(({ url, text }) => (
-                    <li key={url}>{text}</li>
-                ))}
-            </ul>
-        </Main>
+        <ul style={{ color: "white" }}>
+            {items.map(({ url, text }) => (
+                <li key={url}>{text}</li>
+            ))}
+        </ul>
     );
 };
 
-export default IndexPage;
+const Poem: React.FC = () => {
+    return (
+        <PoemC>
+            <PoemText />
+        </PoemC>
+    );
+};
 
-export const Head: HeadFC = () => <title>Home</title>;
+const PoemC = styled.p`
+    margin: 40svh 2rem;
+    font-family: serif;
+    display: flex;
+`;
+
+const PoemText: React.FC = () => {
+    return (
+        <p>
+            <i>murmur</i> to me softly
+            <br />
+            &nbsp;&nbsp;tell me it is <i>all right</i>
+            <br />
+            in the <i>wind</i> rustle leaves
+            <br />
+            &nbsp;&nbsp;the moon, and the <i>night</i>
+        </p>
+    );
+};
