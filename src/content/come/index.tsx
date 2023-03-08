5d4fc82028f9446946b96b2d66d1d0956d76ea9d
Manav <manav@mrmr.io>
2023-03-08 08:19:12 +0530

    Move to ts

---
+++ src/content/come/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Column } from "components/Column";
+
+export const Page: React.FC = () => {
+    return (
+        <Column>
+            <h1>*come dream w*i*th me*</h1>
+            <p>the best is yet to be</p>
+        </Column>
+    );
+};
