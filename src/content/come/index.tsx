d83fa95fe63ef53b3056540964c170058c4f2c45
Manav <manav@mrmr.io>
2023-03-08 08:47:01 +0530

    Style

--- src/content/come/index.tsx
+++ src/content/come/index.tsx
@@ -1,11 +1,30 @@
 import * as React from "react";
 import { Column } from "components/Column";
+import styled from "styled-components";
 
 export const Page: React.FC = () => {
     return (
         <Column>
-            <h1>*come dream w*i*th me*</h1>
-            <p>the best is yet to be</p>
+            <H1>
+                come dream
+                <br />
+                with me
+            </H1>
+            <P>the best is yet to be</P>
         </Column>
     );
 };
+
+const H1 = styled.h1`
+    margin: 1.8rem;
+    margin-top: 2rem;
+    margin-bottom: 1.3rem;
+    font-weight: 800;
+    font-style: italic;
+`;
+
+const P = styled.p`
+    margin: 1.8rem;
+    margin-top: 1.3rem;
+    font-weight: 300;
+`;
