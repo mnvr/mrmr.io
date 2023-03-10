19f2c1774201d0dbfd60a1b647d6689703a904b6
Manav <manav@mrmr.io>
2023-03-10 21:02:42 +0530

    Add vis for come

--- src/content/come/vis.ts
+++ src/content/come/vis.ts
@@ -3,6 +3,8 @@ import HydraRenderer from "hydra-synth";
 
 export const vis = ({ synth: h }: HydraRenderer) => {
     h.hsl(248.2 / 360, 0.991, 0.555)
+        .add(h.osc())
+        .rotate(() => Math.sin(h.time * 0.1))
         .aspect()
         .out();
 };
