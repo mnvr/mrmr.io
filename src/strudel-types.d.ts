5bd455bedb6c0b623f524be99ee713ba06031206
Manav <manav@mrmr.io>
2023-03-12 04:33:45 +0530

    Start patterning

--- src/strudel-types.d.ts
+++ src/strudel-types.d.ts
@@ -1,1 +1,49 @@
-declare module "@strudel.cycles/core" {}
+declare module "@strudel.cycles/core" {
+    /**
+     * Patterns are the core abstraction of Tidal(Cycles).
+     *
+     * A Pattern is something that can be queried to ask it which Events (aka
+     * {@link Hap}s) occur during a given Timespan. {@link Timespan}s are spans
+     * of cycles. By convention, 1 cycle maps to 1 second of simulated time.
+     */
+    export class Pattern {}
+
+    /**
+     * A pattern, or something which can be converted into a pattern.
+     */
+    export type Patternable = Pattern | string;
+
+    /**
+     * Concatenate the patterns, one each per cycle.
+     *
+     * Concatenate the given patterns to form a new {@link Pattern} that cycles
+     * through the given patterns one by one, with each pattern taking one
+     * cycle.
+     *
+     * Constrast with {@link seq}, where all the patterns are squished into a
+     * single cycle.
+     *
+     * @synonyms {@link slowcat}
+     */
+    export const cat: (...pats: Patternable[]) => Pattern;
+    /** @see {@link cat} */
+    export const slowcat: (...pats: Patternable[]) => Pattern;
+
+    /**
+     * Sequence the patterns subdividing a cycle equally between them.
+     *
+     * Sequence the given patterns to form a new {@link Pattern} that cycles
+     * through the given patterns one by one, such that going through the
+     * sequence of all of them together still takes one cycle.
+     *
+     * Contrast with {@link cat}, where each of the patterns individually takes
+     * a single cycle.
+     *
+     * @synonyms {@link fastcat}, {@link seq}
+     */
+    export const sequence: (...pats: Patternable[]) => Pattern;
+    /** @see {@link sequence} */
+    export const fastcat: (...pats: Patternable[]) => Pattern;
+    /** @see {@link sequence} */
+    export const seq: (...pats: Patternable[]) => Pattern;
+}
--- src/strudel/init.ts
+++ src/strudel/init.ts
@@ -1,18 +1,37 @@
-// import { sequence } from "@strudel.cycles/core";
+import { sequence } from "@strudel.cycles/core";
 
 /**
  * ## Strudel
+ *
+ * Tidal for JavaScript.
  *
  * ### Installation
  *
  * Strudel is split into a number of small packages. For our purposes, we need
  * the following:
- * - `@strudel.cycles/core`
+ * - `@strudel.cycles/core` - as it says on the tin
+ * - TODO `@strudel.cycles/mini` - to allow us to use the mini notation
+ * - TODO `@strudel.cycles/webaudio` - to allow us to emit sounds using
+ *   [WebAudio](https://www.w3.org/TR/webaudio/)
  *
  * ### Core
  *
- * ...
+ * The heart of Tidal is the {@link Pattern} class. The core contains various
+ * primitive pattern (notes, silence etc), and ways to combine them (concatenate
+ * them, stack them, add them etc).
+ *
+ * ### Mini
+ *
+ * The mini notation is syntax sugar to simplify writing patterns.
+ *
+ * ### WebAudio
+ *
+ * A pattern is something you can query to ask what all events happen over a
+ * cycle (By convention, 1 cycle is 1 second). Events are arbitrary things, but
+ * particularly are things that can be rendered as audio using WebAudio.
+ *
  */
 export const test = () => {
-    console.log("test");
+    const pattern = sequence("a", ["b", "c"]);
+    console.log("pattern", pattern);
 };
