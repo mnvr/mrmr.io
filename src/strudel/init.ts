745bcca85d42446562a3e2211d85b01bbb8e51ee
Manav <manav@mrmr.io>
2023-03-12 16:56:13 +0530

    Document controls

--- src/strudel/init.ts
+++ src/strudel/init.ts
@@ -1,4 +1,4 @@
-import { sequence } from "@strudel.cycles/core";
+import { controls, Pattern, sequence } from "@strudel.cycles/core";
 
 /**
  * ## Strudel
@@ -20,6 +20,10 @@ import { sequence } from "@strudel.cycles/core";
  * primitive pattern (notes, silence etc), and ways to combine them (concatenate
  * them, stack them, add them etc).
  *
+ * Control parameters act as FX chains, modifying patterns to create new ones
+ * that incorporate the effects of the control parameter. The control parameter
+ * itself can be parameterized by a pattern to change its value.
+ *
  * ### Mini
  *
  * The mini notation is syntax sugar to simplify writing patterns.
@@ -28,14 +32,31 @@ import { sequence } from "@strudel.cycles/core";
  *
  * A pattern is something you can query to ask what all events happen over a
  * cycle (By convention, 1 cycle is 1 second). Events can be arbitrary things,
- * but the ones we're dealing with are things that can be rendered as audio
- * by the WebAudio renderer.
+ * but the ones we're dealing with are things that can be rendered as audio by
+ * the WebAudio renderer.
  *
  */
 export const test = () => {
-    const pattern = sequence("a", ["b", "c"]);
+    const pattern = sequence("a", ["b", "f5"]);
     console.log("pattern", pattern);
     pattern.drawLine();
     const events = pattern.queryArc(0, 2);
+    events.forEach((e) => console.log(e.show()));
+
+    // @ts-ignore
+    debugPrint(controls.note(pattern));
+
+    console.log(controls);
+
+    const { note } = controls;
+    debugPrint(note("a"));
+
+    debugPrint(note("f").cutoff(sequence(500, 900)), "prefix")
+    debugPrint(sequence(1).note("f").cutoff(sequence(500, 900)), "inline")
+};
+
+const debugPrint = (pattern: Pattern, preamble?: string) => {
+    const events = pattern.queryArc(0, 1);
+    if (preamble) console.log(preamble)
     events.forEach((e) => console.log(e.show()));
 };
