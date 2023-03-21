f1119c00bc61108f247c8ffe490edbdb7e7d558d
Manav <manav@mrmr.io>
2023-03-21 19:35:36 +0530

    Ring

--- users/mnvr/come/song.ts
+++ users/mnvr/come/song.ts
@@ -5,10 +5,20 @@ import type { TidalSong } from "types";
 export const song: TidalSong = () => {
     const { note } = controls;
 
-    const p1 = note(m`c <a ~ ~ [f e]>`);
+    const p1 = note(m`c <a ~ ~ [f e]>`).gain(0.5);
     const p2 = note(m`c <a ~ ~ [f e]>`)
         .add(12)
-        .s("sawtooth")
-        .gain(0.1);
-    return stack(p1, p2);
+        .s("tri")
+        .gain(0.3);
+    const p3 = stack(p1, p2);
+
+    const c1 = note(m`[c5*5 d5*7] <c6 g#6 g6 c7>`)
+        .cutoff(m`1500 3000 400 1000 9000`)
+        .sustain(m`1.3 0.7 1.6`)
+        .delay([0.4, 0.9, 0])
+        .gain(0.7);
+
+    const p4 = stack(c1);
+
+    return p4;
 };
