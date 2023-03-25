818e00a3b47e6eb31d1aecb838f8682eccc01c88
Manav <manav@mrmr.io>
2023-03-25 11:49:35 +0530

    Slow cutoff

--- users/mnvr/come/song.ts
+++ users/mnvr/come/song.ts
@@ -1,4 +1,4 @@
-import { controls, stack } from "@strudel.cycles/core";
+import { controls, mask, sine, stack } from "@strudel.cycles/core";
 import { m } from "strudel/mini";
 import type { TidalSong } from "types";
 
@@ -12,12 +12,14 @@ export const song: TidalSong = () => {
         .slow(8)
         .sub(note(12));
 
-    const d3 = note(m`g!6 f g`)
+    const d3 = note(
+        m`[g!6 f g]!5 [f!6 g f]!2 [d!6 g f] [[c | d]!6 f d] [c@3!2 d@2]!2`
+    )
+        .slow(11)
         .add(note(12 + 7))
+        .cutoff(sine.range(900, 20000).slow(30))
         .gain(0.5)
-        .fast(40)
-        .mask(m`0 1!3`)
-        .slow(40);
+        .outside(20, mask(m`1 1!4`));
 
     return stack(d1.hush(), d2.hush(), d3);
 };
