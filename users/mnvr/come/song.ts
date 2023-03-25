1a8eb8d7ddaf8f608fd39e9f19cfd86d7d61c68e
Manav <manav@mrmr.io>
2023-03-25 10:58:23 +0530

    C2

--- users/mnvr/come/song.ts
+++ users/mnvr/come/song.ts
@@ -5,7 +5,13 @@ import type { TidalSong } from "types";
 export const song: TidalSong = () => {
     const { note } = controls;
 
-    const d1 = note(m`c d f a`);
+    const d1 = note(
+        m`[c d f a]!7 [g!6 f g] [c d f a]!7 [g g g@2 g@2 f g]`
+    ).slow(16);
+    const d2 = note(m`[c d f a]!7 [g f d c]`)
+        .slow(8)
+        .sub(note(12));
+    const d3 = note(m`g!6 f g`).add(note(12 + 7)).gain(0.5);
 
-    return stack(d1);
+    return stack(d1, d2, d3);
 };
