31d35c908ee3281a2fce213c94f262c3de1a7af7
Manav <manav@mrmr.io>
2023-03-30 17:28:52 +0530

    Add workaround for Safari errors
    Elsewhere:
    * https://stackoverflow.com/questions/74463640/safari-invalid-value-for-svg-attribute-width-height-rem
    * https://stackoverflow.com/questions/54683766/angular-5-2-error-invalid-value-for-svg-attribute-height-1-25rem-from-pl

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -16,9 +16,26 @@ export const IconButton = styled.button`
 `;
 
 export const PlayButton: React.FC = () => {
+    // Safari doesn't support rem units on SVG elements
+    // So this could've just been
+    //
+    //     <IconButton>
+    //         <BsPlayFill size="2rem" title="Play" />
+    //     </IconButton>
+    //
+    // Except that causes Safari to log errors to the console. So we instead
+    // wrap the icon in a container that sets the font size to 2rem;
+
     return (
         <IconButton>
-            <BsPlayFill size="2rem" title="Play" />
+            <IconWrapper2rem>
+                <BsPlayFill title="Play" />
+            </IconWrapper2rem>
         </IconButton>
     );
 };
+
+const IconWrapper2rem = styled.div`
+    /* Workaround - Safari doesn't support rem units on SVG elements */
+    font-size: 2rem;
+`;
