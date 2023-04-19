d10c4cf43792cf386222bf2c99ab435e9be734bd
Manav <manav@mrmr.io>
2023-04-19 18:47:39 +0530

    Clean up

--- users/mnvr/rocks/index.tsx
+++ users/mnvr/rocks/index.tsx
@@ -1,6 +1,5 @@
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import * as React from "react";
-import styled from "styled-components";
 import { BuildTimePageContext, type Page } from "templates/page";
 import { ensure } from "utils/ensure";
 
@@ -12,22 +11,9 @@ export const Content: React.FC = () => {
 
 const RockGold: React.FC<{ page: Page }> = ({ page }) => {
     const image = ensure(getImage(ensure(page.images["gold"])));
-    console.log(image);
-    return (
-        <ImageContainer>
-            <GatsbyImage image={image} alt="Rust and gold colored rock face" />
-        </ImageContainer>
-    );
+
+    return <GatsbyImage image={image} alt="Rust and gold colored rock face" />;
 };
-
-const ImageContainer = styled.div`
-    /* height: 100vh; */
-`;
-
-const Image = styled.img`
-    width: 100%;
-    /* min-height: 100vh; */
-`;
 
 const Title: React.FC = () => {
     return <h1>Rocks</h1>;
