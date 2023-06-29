0eb7c1e4883434b5da0bc152b9bbd50035e240f1
Manav <manav@mrmr.io>
2023-06-29 13:09:27 +0530

    Increase height to take more of the screen on mobile devices

--- pages/c1/index.tsx
+++ pages/c1/index.tsx
@@ -26,11 +26,17 @@ const enableTestRecording = false;
 const SketchBox: React.FC = () => {
     const [recorder, _] = React.useState(new VideoRecorder());
 
-    const height = 500;
+    // Instagram's recommended Reel size is 1080x1920 pixels (9:16 aspect ratio)
+    // For @3x devices, that'll translate to 1920/3 = 640 points, and we use
+    // that as the height. However, if the window is smaller than that, we limit
+    // to the window's height.
+    const defaultHeight = 640;
     const aspectRatio = 9 / 16;
-    const width = height * aspectRatio;
 
     const wrappedSetup = (p5: p5Types, canvasParentRef: Element) => {
+        const height = Math.min(defaultHeight, p5.windowHeight);
+        const width = height * aspectRatio;
+
         // Use the `parent` method to ask p5 render to the provided canvas ref
         // instead of creating and rendering to a canvas of its own.
         p5.createCanvas(width, height).parent(canvasParentRef);
