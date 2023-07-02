0f604a53889b4845dc634aa92bdb0dad56f8724b
Manav <manav@mrmr.io>
2023-07-02 16:58:10 +0530

    Use clear instead of background

--- pages/c1/sketch.tsx
+++ pages/c1/sketch.tsx
@@ -1,12 +1,17 @@
-import type p5Types from "p5";
+import p5Types from "p5";
 
 let gd = 0;
 
 export const setup = (p5: p5Types) => {};
 
 export const draw = (p5: p5Types) => {
-    p5.background("lightblue");
-    p5.stroke(102);
+    // Using clear instead background here because clear clears out the canvas
+    // to a transparent background. This way, we do not need to specify the
+    // background color as a p5.Color, and can use a more vibrant color
+    // specified e.g. using OKLCH.
+    p5.clear();
+
+    p5.stroke(255);
     {
         const gap = 20;
         for (let i = 0; i < (p5.width / gap) * 2; i += 1) {
@@ -14,23 +19,12 @@ export const draw = (p5: p5Types) => {
         }
     }
     {
-        const gap = 22 + gd; // + Math.sin(p5.frameCount / 70) / 2;
+        const gap = 22 + gd + Math.sin(p5.frameCount / 70) / 2;
         // p5.translate(p5Types.Vector.fromAngle(p5.frameCount / 1, 4));
-        // p5.translate(-71, 1, 0);
-        // p5.translate(0, -10, 0);
         // p5.translate(p5Types.Vector.fromAngle(p5.PI / 0, 19));
-        // p5.translate(71, 71, 0);
         // p5.translate(p5.createVector(70.71067811865476, 70.71067811865474, 0));
-
-        // console.log(p5Types.Vector.fromAngle(p5.PI / 4, 10));
-        // console.log(p5Types.Vector.fromAngle(p5.frameCount / 1, 4));
-        // console.log(gd);
         for (let i = 0; i < p5.width / gap; i += 1) {
             p5.line(2 + i * gap, 0, 2 + i * gap, p5.height);
         }
-        // p5.circle(50, 50, 10);
-        // p5.translate(p5Types.Vector.fromAngle(p5.PI / 0, 19));
-        // p5.stroke(p5Types.Color());
-        p5.circle(150, 50, 10);
     }
 };
