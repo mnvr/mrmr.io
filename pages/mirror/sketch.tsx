5c4f7a7bb1cc691a534e00cb0f6e8c342a1a4090
Manav <manav@mrmr.io>
2023-07-05 17:18:03 +0530

    Tame the rotation

--- pages/mirror/sketch.tsx
+++ pages/mirror/sketch.tsx
@@ -29,14 +29,33 @@ const gridCircles = (p5: p5Types) => {
 
     p5.noFill();
 
+    let c = 0;
+
+    p5.rectMode(p5.CENTER);
+    p5.angleMode(p5.RADIANS);
+
     const gap = 40;
     const offset = 20;
     for (let y = gap + offset; y < p5.height - offset; y += gap) {
         for (let x = gap + offset; x < p5.width - offset; x += gap) {
-            const yi = y / gap;
-            const xi = x / gap;
-
-            p5.circle(x, y, 12);
+            // Alternate between the circle and the star
+            if (c++ % 2) p5.circle(x, y, 12);
+            else curvedStar(p5, x, y);
         }
     }
+};
+
+const curvedStar = (p5: p5Types, x: number, y: number) => {
+    p5.push();
+    // Translate to the center so that the rotation happens around it
+    //
+    // From
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/rotate:
+    // > The rotation center point is always the canvas origin. To change the
+    //   center point, you will need to move the canvas by using the
+    //   `translate` method.
+    p5.translate(x, y);
+    p5.rotate(p5.PI / 4);
+    p5.rect(0, 0, 20, 20);
+    p5.pop();
 };
