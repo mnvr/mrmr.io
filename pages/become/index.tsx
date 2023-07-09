a15dc9d5ff0e1be148dea5984ed11063d875f6e9
Manav <manav@mrmr.io>
2023-07-09 23:15:06 +0530

    Add the poem text

--- pages/become/index.tsx
+++ pages/become/index.tsx
@@ -1,6 +1,7 @@
-import { FooterA } from "components/FooterA";
 import { PlayerP5WebAudio } from "components/PlayerP5WebAudio";
+import { Link } from "gatsby";
 import * as React from "react";
+import styled from "styled-components";
 import { BuildTimePageContext } from "templates/page";
 import { ensure } from "utils/ensure";
 import { onlyValue } from "utils/object";
@@ -13,7 +14,74 @@ export const Content: React.FC = () => {
     return (
         <div>
             <PlayerP5WebAudio draw={draw} songURL={ensure(onlyValue(mp3s))} />
-            <FooterA />
+            <Description />
         </div>
     );
 };
+
+const Poem: React.FC = () => {
+    return (
+        <p>
+            To all of you stuck in the words
+            <br />
+            of dead poets
+            <br />
+            Go out and find the living ones
+            <br />
+            And if you can’t find them
+            <br />
+            Become
+            <br />
+        </p>
+    );
+};
+
+export const Description: React.FC = () => {
+    return (
+        <>
+            <PoemContainer>
+                <Poem />
+            </PoemContainer>
+            <FooterContainer>
+                <FooterContents>
+                    <small>
+                        <span className="link-prelude">
+                            music, words and art by{" "}
+                        </span>
+                        <Link to="/">manav</Link>
+                    </small>
+                </FooterContents>
+            </FooterContainer>
+        </>
+    );
+};
+
+const PoemContainer = styled.main`
+    display: grid;
+    place-content: center start;
+    min-height: 80svh;
+    padding: 1.3rem;
+
+    line-height: 1.4rem;
+`;
+
+const FooterContainer = styled.footer`
+    padding: 1.3rem;
+    margin-block-end: 4rem;
+`;
+
+const FooterContents = styled.div`
+    .link-prelude {
+        opacity: 0.7;
+    }
+
+    a {
+        text-decoration: none;
+        opacity: 0.7;
+        border-bottom: 1px solid currentColor;
+    }
+
+    a:hover {
+        opacity: 1;
+    }
+`;
