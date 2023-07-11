f115b193c8e4a5daa851f6c510ddf68afba6caec
Manav <manav@mrmr.io>
2023-07-11 20:21:48 +0530

    Extract

--- pages/become/sequencer.tsx
+++ pages/become/sequencer.tsx
@@ -1,5 +1,4 @@
-import { ensure } from "utils/ensure";
-import { loadAudioBuffer } from "webaudio/audio";
+import { createHelpers } from "webaudio/audio";
 
 export const createSequencer = (mp3s: Record<string, string>) => {
     return async (ctx: AudioContext) => {
@@ -25,12 +24,12 @@ export const createSequencer = (mp3s: Record<string, string>) => {
 
         // loop...
         const loop = (at: number) => {
-            const cp = source(pad);
-            cp.onended = (ev: Event) => {
+            const sp = source(pad);
+            sp.onended = (ev: Event) => {
                 // Schedule the next to next one
                 loop(at + 2 * d);
             };
-            cp.start(at);
+            sp.start(at);
             source(drums).start(at);
             source(bass).start(at);
         };
@@ -38,59 +37,4 @@ export const createSequencer = (mp3s: Record<string, string>) => {
         loop(2 * d);
         loop(3 * d);
     };
-};
-
-interface SequencerHelpers {
-    /**
-     * A function to load a sample into an audio buffer (async)
-     *
-     * @param name The name of the sample. This must be one of the keys in the
-     * `mp3s` Record that is passed to {@link createHelpers}.
-     */
-    load: (name: string) => Promise<AudioBuffer>;
-    /**
-     * A mixer connected to the destination of the audio context passed to
-     * {@link createHelpers}.
-     *
-     * Audio playback nodes created by other methods like {@link play} will
-     * connect to this mixer. This thus acts as a funnel, so gain of this mixer
-     * can thus be changed to effect the overall volume, or it can be fed into
-     * an analyser node.
-     */
-    mixer: GainNode;
-    /**
-     * Create a node to play the given buffer, and connect it to the
-     * {@link mixer}.
-     */
-    source: (buffer: AudioBuffer) => AudioBufferSourceNode;
-}
-/**
- * Create various routines that reduce the amount of code we've to write when
- * creating the audio graph in common types of sequencers.
- *
- * @param mp3s The URLs of the MP3 files associated with the page, indexed by
- * their name.
- *
- * @param ctx The {@link AudioContext} in which playback will occur.
- *
- * @returns SequencerHelpers
- */
-const createHelpers = (
-    mp3s: Record<string, string>,
-    ctx: AudioContext
-): SequencerHelpers => {
-    const load = (name: string) => loadAudioBuffer(ctx, ensure(mp3s[name]));
-
-    const mixer = new GainNode(ctx);
-    mixer.connect(ctx.destination);
-
-    const source = (b: AudioBuffer) => {
-        const bufferSource = new AudioBufferSourceNode(ctx, {
-            buffer: b,
-        });
-        bufferSource.connect(mixer);
-        return bufferSource;
-    };
-
-    return { load, mixer, source };
 };
--- src/webaudio/audio.ts
+++ src/webaudio/audio.ts
@@ -1,3 +1,5 @@
+import { ensure } from "utils/ensure";
+
 /**
  * An async function to construct the WebAudio graph
  *
@@ -32,6 +34,62 @@ export const createLoopSequencer = (loopURL: string) => {
         const audioBuffer = await loadAudioBuffer(audioContext, loopURL);
         loopAudioBuffer(audioContext, audioBuffer);
     };
+};
+
+export interface SequencerHelpers {
+    /**
+     * A function to load a sample into an audio buffer (async)
+     *
+     * @param name The name of the sample. This must be one of the keys in the
+     * `mp3s` Record that is passed to {@link createHelpers}.
+     */
+    load: (name: string) => Promise<AudioBuffer>;
+    /**
+     * A mixer connected to the destination of the audio context passed to
+     * {@link createHelpers}.
+     *
+     * Audio playback nodes created by other methods like {@link play} will
+     * connect to this mixer. This thus acts as a funnel, so gain of this mixer
+     * can thus be changed to effect the overall volume, or it can be fed into
+     * an analyser node.
+     */
+    mixer: GainNode;
+    /**
+     * Create a node to play the given buffer, and connect it to the
+     * {@link mixer}.
+     */
+    source: (buffer: AudioBuffer) => AudioBufferSourceNode;
+}
+
+/**
+ * Create various routines that reduce the amount of code we've to write when
+ * creating the audio graph in common types of sequencers.
+ *
+ * @param mp3s The URLs of the MP3 files associated with the page, indexed by
+ * their name.
+ *
+ * @param ctx The {@link AudioContext} in which playback will occur.
+ *
+ * @returns SequencerHelpers
+ */
+export const createHelpers = (
+    mp3s: Record<string, string>,
+    ctx: AudioContext
+): SequencerHelpers => {
+    const load = (name: string) => loadAudioBuffer(ctx, ensure(mp3s[name]));
+
+    const mixer = new GainNode(ctx);
+    mixer.connect(ctx.destination);
+
+    const source = (b: AudioBuffer) => {
+        const bufferSource = new AudioBufferSourceNode(ctx, {
+            buffer: b,
+        });
+        bufferSource.connect(mixer);
+        return bufferSource;
+    };
+
+    return { load, mixer, source };
 };
 
 /**
@@ -82,7 +140,6 @@ export const loadAudioBuffer = async (
     const response = await fetch(url);
     const arrayBuffer = await response.arrayBuffer();
     const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
-    console.log("Audio buffer loaded", url);
     return audioBuffer;
 };
 
