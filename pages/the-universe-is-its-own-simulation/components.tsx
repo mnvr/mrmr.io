404e8c7aead5dbd218cd6f55c615121a869ef0dc
Manav <manav@mrmr.io>
2023-10-06 16:47:21 +0530

    Add title

--- pages/the-universe-is-its-own-simulation/components.tsx
+++ pages/the-universe-is-its-own-simulation/components.tsx
@@ -1,11 +1,41 @@
 import ReactP5Wrapper from "p5/ReactP5Wrapper";
 import * as React from "react";
+import styled from "styled-components";
+import { BuildTimePageContext } from "templates/page";
+import { ensure } from "utils/ensure";
 import { sketch } from "./sketch";
-
-export const Content: React.FC = () => {
-    return <div />; //<P5Layout draw={draw} />;
-};
 
 export const Sketch: React.FC = () => {
     return <ReactP5Wrapper sketch={sketch} />;
 };
+
+export const Title: React.FC = () => {
+    const page = ensure(React.useContext(BuildTimePageContext));
+    const { formattedDateMY } = page;
+
+    return (
+        <TitleContainer>
+            <h1>
+                The Universe Is
+                <br />
+                Its Own Simulation
+            </h1>
+            <div>
+                <Caption>
+                    Manav Rathi
+                    <br />
+                    {formattedDateMY}
+                </Caption>
+            </div>
+        </TitleContainer>
+    );
+};
+
+const TitleContainer = styled.div`
+    margin-inline: auto;
+    text-align: center;
+`;
+
+const Caption = styled.small`
+    color: oklch(93.58% 0.12 151);
+`;
