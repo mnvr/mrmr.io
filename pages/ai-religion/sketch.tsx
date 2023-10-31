cb6ee8e69b5e7c619e28fe32e462ee7236da3194
Manav <manav@mrmr.io>
2023-10-31 12:43:11 +0530

    Add simple fragment shader

--- pages/ai-religion/sketch.tsx
+++ pages/ai-religion/sketch.tsx
@@ -1,16 +1,28 @@
 import type { Sketch } from "@p5-wrapper/react";
 
+const vertexShader = `
+attribute vec3 aPosition;
+
+void main() {
+  gl_Position = vec4(aPosition, 1.0);
+}`;
+
+const fragmentShader = `
+precision mediump float;
+
+void main() {
+  vec2 uv = gl_FragCoord.xy / 600.0;
+
+  gl_FragColor = vec4(uv.xy, 0.0, 1.0);
+}`;
+
 export const sketch: Sketch = (p5) => {
-    p5.setup = () => p5.createCanvas(300, 300, p5.WEBGL);
+    p5.setup = () => {
+        p5.createCanvas(300, 300, p5.WEBGL);
+        p5.shader(p5.createShader(vertexShader, fragmentShader));
+    };
 
     p5.draw = () => {
-        p5.background(250);
-        p5.normalMaterial();
-        p5.push();
-        p5.rotateZ(p5.frameCount * 0.01);
-        p5.rotateX(p5.frameCount * 0.01);
-        p5.rotateY(p5.frameCount * 0.01);
-        p5.plane(100);
-        p5.pop();
+        p5.quad(-1, -1, 1, -1, 1, 1, -1, 1);
     };
 };
