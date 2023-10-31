12cbc79a1d9040d444a164fe77df651e9e750a9e
Manav <manav@mrmr.io>
2023-10-31 14:55:36 +0530

    Wide

--- pages/ai-religion/sketch.tsx
+++ pages/ai-religion/sketch.tsx
@@ -16,14 +16,13 @@ uniform float u_time;
 
 void main() {
     vec2 uv = (2. * gl_FragCoord.xy - u_resolution) / min(u_resolution.x, u_resolution.y);
-    // A circle of radius 0.8
+    // A circle of some radius
     float sdf = length(uv) - 0.8;
     float d = abs(sdf);
     // Use the reciprocal to give a neon vibe.
     float r = 0.02 + sin(u_time / 2.5) * 0.007;
     float c = r / (d + 0.001);
 
-    // c = 1.0 - c;
     c = 1.0 - smoothstep(c, 0.01, 0.02);
 
     gl_FragColor = vec4(c, c, c, 1.0);
@@ -33,7 +32,7 @@ export const sketch: Sketch = (p5) => {
     let shader: p5.Shader;
 
     p5.setup = () => {
-        p5.createCanvas(300, 300, p5.WEBGL);
+        p5.createCanvas(p5.min(p5.windowWidth, 600), 300, p5.WEBGL);
 
         shader = p5.createShader(vertexShader, fragmentShader);
         p5.shader(shader);
