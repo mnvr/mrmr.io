67c29d20b8dfaf1d8564c528eceeb2e54218dede
Manav <manav@mrmr.io>
2023-10-31 14:44:43 +0530

    Breathe

--- pages/ai-religion/sketch.tsx
+++ pages/ai-religion/sketch.tsx
@@ -1,4 +1,5 @@
 import type { Sketch } from "@p5-wrapper/react";
+import p5 from "p5";
 
 const vertexShader = `
 attribute vec3 aPosition;
@@ -11,6 +12,7 @@ const fragmentShader = `
 precision mediump float;
 
 uniform vec2 u_resolution;
+uniform float u_time;
 
 void main() {
     vec2 uv = (2. * gl_FragCoord.xy - u_resolution) / min(u_resolution.x, u_resolution.y);
@@ -18,24 +20,30 @@ void main() {
     float sdf = length(uv) - 0.8;
     float d = abs(sdf);
     // Use the reciprocal to give a neon vibe.
-    float c = 0.01 / (d + 0.01);
+    float r = 0.02 + sin(u_time / 2.5) * 0.007;
+    float c = r / (d + 0.001);
+
+    // c = 1.0 - c;
+    c = 1.0 - smoothstep(c, 0.01, 0.02);
 
     gl_FragColor = vec4(c, c, c, 1.0);
 }`;
 
 export const sketch: Sketch = (p5) => {
+    let shader: p5.Shader;
+
     p5.setup = () => {
         p5.createCanvas(300, 300, p5.WEBGL);
 
-        const shader = p5.createShader(vertexShader, fragmentShader);
+        shader = p5.createShader(vertexShader, fragmentShader);
         p5.shader(shader);
-        // Since the sketch dimensions don't change, we can set it once and for
-        // all here in setup instead of updating it in draw.
-        const pd = p5.pixelDensity();
-        shader.setUniform("u_resolution", [p5.width * pd, p5.height * pd]);
     };
 
     p5.draw = () => {
+        const pd = p5.pixelDensity();
+        shader.setUniform("u_resolution", [p5.width * pd, p5.height * pd]);
+        shader.setUniform("u_time", p5.millis() / 1000);
+
         p5.quad(-1, -1, 1, -1, 1, 1, -1, 1);
     };
 };
