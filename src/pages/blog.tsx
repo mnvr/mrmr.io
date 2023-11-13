371dda760a553f230ec84f9d07099f7fc44ac76f
Manav <manav@mrmr.io>
2023-11-13 13:49:04 +0530

    Try date based sectioning

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -42,9 +42,7 @@ export const Head: HeadFC = ({}) => {
 export const query = graphql`
     query BlogPage {
         allMdx(
-            filter: {
-                frontmatter: { tags: { in: "blog" }, unlisted: { ne: true } }
-            }
+            filter: { frontmatter: { unlisted: { ne: true } } }
             sort: [
                 { frontmatter: { date: DESC } }
                 { frontmatter: { title: ASC } }
@@ -54,6 +52,7 @@ export const query = graphql`
                 frontmatter {
                     title
                     description
+                    formattedDateMY: date(formatString: "MMM YYYY")
                 }
                 fields {
                     slug
@@ -89,6 +88,7 @@ interface Page {
     title: string;
     slug: string;
     description?: string;
+    formattedDateMY: string;
 }
 
 const parsePages = (data: Queries.BlogPageQuery) => {
@@ -101,8 +101,9 @@ const parsePages = (data: Queries.BlogPageQuery) => {
 
         const title = ensure(frontmatter?.title);
         const description = frontmatter?.description;
+        const formattedDateMY = ensure(frontmatter?.formattedDateMY);
 
-        return { title, slug, description };
+        return { slug, title, description, formattedDateMY };
     });
 };
 
@@ -112,14 +113,38 @@ interface PageListingProps {
 }
 
 const PageListing: React.FC<PageListingProps> = ({ pages }) => {
-    const n = pages.length;
+    const pageOrDates = sectionByMonth(pages);
     return (
         <PageListing_>
-            {pages.map((page) => (
-                <PageItem key={page.slug} {...page} />
-            ))}
+            {pageOrDates.map((e) =>
+                typeof e === "string" ? (
+                    <SectionHeader key={e}>{e}</SectionHeader>
+                ) : (
+                    <PageItem key={e.slug} {...e} />
+                ),
+            )}
         </PageListing_>
     );
+};
+
+type PageOrDate = Page | string;
+
+/**
+ * Convert a linear list of pages into one where pages with a creation date
+ * within the same calendar month are grouped together by interspersing the
+ * original list of pages by strings representing the section titles (the month
+ * name)
+ */
+const sectionByMonth = (pages: Page[]): PageOrDate[] => {
+    let currentDate: string | undefined;
+    let result: PageOrDate[] = [];
+    pages.forEach((page) => {
+        if (page.formattedDateMY !== currentDate)
+            result.push((currentDate = page.formattedDateMY));
+        result.push(page);
+    });
+    console.log(result);
+    return result;
 };
 
 const PageListing_ = styled.ul`
@@ -132,13 +157,24 @@ const PageListing_ = styled.ul`
 
     a {
         text-decoration: none;
-        border-bottom: 1px solid blue;
+        border-bottom: 2px solid blue;
         font-weight: 600;
     }
 
-    a:visited {
-        border-bottom: 1px solid currentColor;
+    li {
+        margin-block: 1rem;
     }
+    li:nth-child(even) {
+        a {
+            border-bottom: 1px solid currentColor;
+        }
+    }
+`;
+
+const SectionHeader = styled.h4`
+    font-style: italic;
+    font-weight: 300;
+    opacity: 0.33;
 `;
 
 const PageItem: React.FC<Page> = ({ title, description, slug }) => {
@@ -146,6 +182,8 @@ const PageItem: React.FC<Page> = ({ title, description, slug }) => {
         <li>
             <Link to={slug}>{title}</Link>.{" "}
             <Description>{description}</Description>
+            {/* <br /> */}
+            {/* <Date>Nov 2023, 780 words</Date> */}
         </li>
     );
 };
@@ -155,4 +193,17 @@ const Description = styled.span`
     font-style: italic;
     font-size: 1.05rem;
     color: var(--mrmr-color-3);
+    margin-bottom: 0;
+    padding-bottom: 0;
+`;
+
+const Date = styled.span`
+    font-family: serif;
+    font-style: italic;
+    font-size: 0.8rem;
+    color: var(--mrmr-color-3);
+    opacity: 0.7;
+    margin-top: 0;
+    padding-top: 0;
+    line-height: 0.5rem;
 `;
