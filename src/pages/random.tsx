21c24f42dd89f4e037d9789b353e3b85729c7387
Manav <manav@mrmr.io>
2023-11-16 11:04:32 +0530

    Clean up /random

--- src/pages/random.tsx
+++ src/pages/random.tsx
@@ -12,10 +12,6 @@ import { replaceNullsWithUndefineds } from "utils/replace-nulls";
  * - Generate a list of all eligible pages at build time.
  * - When the page is loaded, randomly select one of these (on the client side)
  *   and do a client side redirect to it.
- *
- * This process could be inlined in the index page too (since that is where the
- * /random link exists), but that'd slow down the initial load of the index
- * page. So we load this separately.
  */
 const RandomPage: React.FC<PageProps<Queries.RandomPageQuery>> = ({ data }) => {
     const pages = parsePages(data);
@@ -33,19 +29,13 @@ export default RandomPage;
 export const Head: HeadFC = () => <DefaultHead titleSuffix="Random 🎲" />;
 
 /**
- * Fetch all pages, sorted by recency.
+ * Fetch slugs for all pages.
  *
- * - Exclude the pages which are marked `unlisted` (e.g. the "_example" page).
+ * - Exclude the pages which are marked `unlisted`.
  */
 export const query = graphql`
     query RandomPage {
-        allMdx(
-            filter: { frontmatter: { unlisted: { ne: true } } }
-            sort: [
-                { frontmatter: { date: DESC } }
-                { frontmatter: { title: ASC } }
-            ]
-        ) {
+        allMdx(filter: { frontmatter: { unlisted: { ne: true } } }) {
             nodes {
                 fields {
                     slug
