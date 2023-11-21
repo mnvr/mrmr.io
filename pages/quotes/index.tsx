732ed98b385657fd4be5e3dc6b2e0821925afa2f
Manav <manav@mrmr.io>
2023-11-21 16:02:16 +0530

    Sketch out initial take

--- pages/quotes/index.tsx
+++ pages/quotes/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import styled from "styled-components";
 import { ensure } from "utils/ensure";
+import { randomItem } from "utils/random";
 import { quotes } from "./quotes";
 
 export const Content: React.FC = () => {
@@ -16,15 +17,79 @@ const Main = styled.main`
 `;
 
 const Quotes: React.FC = () => {
-    const text = ensure(quotes[0]);
+    const parsedQuotes = parseQuotes(quotes);
+    const randomQuote = ensure(randomItem(parsedQuotes.quotes));
 
-    return <Quote text={text} />;
+    return <Quote quote={randomQuote} parsedQuotes={parsedQuotes} />;
 };
 
-interface QuoteProps {
-    text: string;
+interface ParsedQuotes {
+    quotes: string[];
+    quotesForWord: Map<string, string[]>;
 }
 
-const Quote: React.FC<QuoteProps> = ({ text }) => {
-    return <div>{text}</div>;
+/**
+ * Parse the array of quotes into the {@link ParsedQuotes} data structure which
+ * is amenable to hyperlinking of the form that we want to do here.
+ */
+const parseQuotes = (quotes: string[]): ParsedQuotes => {
+    const quotesForWord = new Map<string, string[]>();
+
+    quotes.forEach((quote) => {
+        words(quote)
+            .filter((w) => !ignoredWords.has(w))
+            .forEach((word) => {
+                quotesForWord.set(
+                    word,
+                    (quotesForWord.get(word) ?? []).concat([word]),
+                );
+            });
+    });
+
+    return {
+        quotes,
+        quotesForWord,
+    };
+};
+
+/** Break a string down into its component words */
+const words = (s: string) => s.split(/\s+/).filter((s) => s);
+
+/**
+ * A set of common / filler words like "is", "the" etc that we ignore when
+ * considering what words to hyperlink.
+ */
+const ignoredWords = new Set<string>(["the", "is", "a"]);
+
+interface QuoteProps {
+    quote: string;
+    parsedQuotes: ParsedQuotes;
+}
+
+const Quote: React.FC<QuoteProps> = ({ quote, parsedQuotes }) => {
+    const { quotesForWord } = parsedQuotes;
+    const spans = words(quote).map((word) => {
+        // If the only link from a word is a single quote, it just points to
+        // itself, so we can ignore it.
+        if ((quotesForWord.get(word)?.length ?? 1) !== 1) {
+            return <a href="#">{word}</a>;
+        } else {
+            return <span>{word}</span>;
+        }
+    });
+    // Intersperse the words with spaces
+    const sentence = spans.reduce(
+        (xs, span) => {
+            return xs.concat([span, <span> </span>]);
+        },
+        [<span />],
+    );
+
+    return (
+        <div>
+            {sentence.map((e, i) => (
+                <span key={i}>{e}</span>
+            ))}
+        </div>
+    );
 };
