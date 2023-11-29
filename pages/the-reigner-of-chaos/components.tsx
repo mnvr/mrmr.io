9feb6efa33a764d358248f12504e9ff988938977
Manav <manav@mrmr.io>
2023-11-29 12:10:14 +0530

    Random elements

--- pages/the-reigner-of-chaos/components.tsx
+++ pages/the-reigner-of-chaos/components.tsx
@@ -2,9 +2,42 @@ import * as React from "react";
 import styled from "styled-components";
 
 export const Sketch: React.FC = () => {
-    return <Sketch_>Test</Sketch_>;
+    const [digits, setDigits] = React.useState<string[] | undefined>();
+
+    React.useEffect(() => {
+        const count = 1000;
+        const numbers = new Uint8Array(count);
+        crypto.getRandomValues(numbers);
+
+        const newDigits = new Array(count);
+        numbers.forEach((n, i) => {
+            newDigits[i] = (n % 10).toString();
+        });
+
+        setDigits(newDigits);
+    }, []);
+
+    if (!digits) return <div />;
+
+    return (
+        <Sketch_>
+            {digits.map((d, i) => {
+                return <Cell key={i}>{d}</Cell>;
+            })}
+        </Sketch_>
+    );
 };
 
 const Sketch_ = styled.div`
     display: grid;
+    grid-template-columns: repeat(40, 1fr);
 `;
+
+interface CellProps {
+    digits: number;
+    i: number;
+}
+
+const Cell: React.FC<React.PropsWithChildren> = ({ children }) => {
+    return <div>{children}</div>;
+};
