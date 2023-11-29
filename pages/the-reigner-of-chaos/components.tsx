9f10ec5b4a9e5923a2edeb60deaf4c0d2f199f7a
Manav <manav@mrmr.io>
2023-11-29 11:50:12 +0530

    Template

---
+++ pages/the-reigner-of-chaos/components.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import styled from "styled-components";
+
+export const Sketch: React.FC = () => {
+    return <Sketch_>Test</Sketch_>;
+};
+
+const Sketch_ = styled.div`
+    display: grid;
+`;
