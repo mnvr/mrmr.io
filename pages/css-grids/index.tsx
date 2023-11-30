12ef9953e8bf97487f67a517e8384c5f6ba1258f
Manav <manav@mrmr.io>
2023-11-30 09:56:21 +0530

    Play with grids
    Following along
    https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Grids

--- pages/css-grids/index.tsx
+++ pages/css-grids/index.tsx
@@ -4,14 +4,30 @@ import styled from "styled-components";
 export const Content: React.FC = () => {
     return (
         <div>
-            <Example1 />
+            <E1 />
+            <E2 />
         </div>
     );
 };
 
 const Bordered = styled.div`
+    p {
+        font-size: smaller;
+        color: var(--mrmr-color-3);
+        margin-block: 0;
+
+        b {
+            color: var(--mrmr-color-1);
+            font-weight: normal;
+        }
+    }
+
     div {
         border: 1px dashed currentColor;
+    }
+
+    div > div {
+        padding: 10px;
     }
 `;
 
@@ -29,10 +45,29 @@ const C6: React.FC<React.HTMLAttributes<HTMLDivElement>> = (props) => {
     );
 };
 
-const Example1: React.FC = () => {
+const E1: React.FC = () => {
     return (
         <Bordered>
-            Default Grid
+            <p>
+                <b>Default Flexbox</b>
+            </p>
+            <DefaultFlex />
+        </Bordered>
+    );
+};
+
+const DefaultFlex = styled(C6)`
+    display: flex;
+`;
+
+const E2: React.FC = () => {
+    return (
+        <Bordered>
+            <p>
+                <b>Default Grid. </b>
+                Unlike Flexbox, the items don't look any different from the
+                normal flow because the default is a one column grid.
+            </p>
             <DefaultGrid />
         </Bordered>
     );
@@ -40,8 +75,4 @@ const Example1: React.FC = () => {
 
 const DefaultGrid = styled(C6)`
     display: grid;
-
-    div {
-        padding: 10px;
-    }
 `;
