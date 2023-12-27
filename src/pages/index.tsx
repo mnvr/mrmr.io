de679d69551ffa009fa9da52bb53c0f9a259d309
Manav <manav@mrmr.io>
2023-12-27 18:49:21 +0530

    Simplify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -289,10 +289,7 @@ const RecentPageDescription = styled.span`
 const Poem: React.FC = () => {
     return (
         <Poem_>
-            <i>
-                <b>m</b>u<b>rm</b>u<b>r</b>
-            </i>{" "}
-            to me softly
+            <i><b>murmur</b></i> to me softly
             <br />
             &nbsp;&nbsp;they tell me <i>it’s all right</i>
             <br />
