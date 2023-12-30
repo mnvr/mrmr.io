613b0c76ade82f49aaacbaa7e6d3c8f1af85bbe6
Manav <manav@mrmr.io>
2023-12-30 20:17:47 +0530

    Start off with an isometric attempt

--- pages/gen24/d/staggered-min/sketch.tsx
+++ pages/gen24/d/staggered-min/sketch.tsx
@@ -1,13 +1,5 @@
-import { gridSketch, type CellShader } from "../../grid";
-
-const staggered = true;
-
-const drawCell: CellShader = ({ p5, x, y, s }) => {
-    p5.fill(170);
-    p5.rect(x, y, s, s);
-};
+import { gridSketch } from "../../grid";
 
 export const sketch = gridSketch({
-    drawCell,
-    staggered,
+    staggered: true,
 });
