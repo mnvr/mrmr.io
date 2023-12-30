40fee023594b53e2d321d0f6daa024642b977fc3
Manav <manav@mrmr.io>
2023-12-30 21:51:15 +0530

    Quad

--- pages/gen24/d/iso-min/sketch.tsx
+++ pages/gen24/d/iso-min/sketch.tsx
@@ -4,7 +4,7 @@ const staggered = true;
 
 const drawCell: CellShader = ({ p5, x, y, s }) => {
     p5.fill(160);
-    p5.rect(x, y, s, s);
+    p5.quad(x, y, x + s / 2, y - s / 2, x + s, y, x + s / 2, y + s / 2);
 };
 
 export const sketch = gridSketch({
