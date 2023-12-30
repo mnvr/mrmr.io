a1ab9a0ed4d9ac88935912035f003880e5e5a75f
Manav <manav@mrmr.io>
2023-12-30 12:36:39 +0530

    Extract the common layout

---
+++ pages/gen24/components.tsx
@@ -0,0 +1,127 @@
+import { type P5WrapperProps } from "@p5-wrapper/react";
+import { LinkStyleUnderlined } from "components/LinkStyles";
+import { Link } from "gatsby";
+import ReactP5WrapperWithFade from "p5/ReactP5WrapperWithFade";
+import * as React from "react";
+import styled from "styled-components";
+
+export const Content: React.FC<React.PropsWithChildren<P5WrapperProps>> = ({
+    sketch,
+    children,
+}) => {
+    return (
+        <Content_>
+            <FirstFold>
+                <Banner />
+                <SketchContainer sketch={sketch} />
+            </FirstFold>
+            {children}
+        </Content_>
+    );
+};
+
+const Content_ = styled.div`
+    display: flex;
+    flex-direction: column;
+    max-width: 900px;
+    margin: auto;
+`;
+
+const FirstFold = styled.div`
+    min-height: 98svh;
+
+    display: flex;
+    flex-direction: column;
+`;
+
+const Banner: React.FC = () => {
+    return (
+        <Banner_>
+            <BannerH />
+        </Banner_>
+    );
+};
+
+const Banner_ = styled.div`
+    height: 67px;
+`;
+
+const SketchContainer: React.FC<P5WrapperProps> = ({ sketch }) => {
+    return (
+        <SketchContainer_>
+            <ReactP5WrapperWithFade sketch={sketch} />
+        </SketchContainer_>
+    );
+};
+
+const SketchContainer_ = styled.div`
+    flex-grow: 1;
+
+    display: flex;
+    justify-content: center; /* horizontally */
+    align-items: center; /* vertically */
+`;
+
+const BannerH: React.FC = () => {
+    return <BannerH_>GEN 24</BannerH_>;
+};
+
+const BannerH_ = styled.h3`
+    @media (max-width: 900px) {
+        margin-inline: 1rem;
+    }
+    color: var(--mrmr-color-3);
+    font-weight: 300;
+`;
+
+export const Description: React.FC<React.PropsWithChildren> = ({
+    children,
+}) => {
+    return (
+        <LinkStyleUnderlined>
+            <Description_>{children}</Description_>
+            <Footer />
+        </LinkStyleUnderlined>
+    );
+};
+
+const Description_ = styled.div`
+    margin-block: 1rem;
+    @media (min-width: 800px) {
+        margin-block: 2rem;
+    }
+    @media (max-width: 900px) {
+        margin-inline: 1rem;
+    }
+
+    max-width: 30rem;
+
+    p {
+        line-height: 1.5rem;
+    }
+
+    a {
+        font-weight: normal;
+    }
+`;
+
+const Footer: React.FC = () => {
+    return (
+        <Footer_>
+            <p>
+                <Link to="/">mrmr.io</Link>
+            </p>
+        </Footer_>
+    );
+};
+
+const Footer_ = styled.div`
+    margin-block: 3rem;
+    @media (max-width: 900px) {
+        margin-inline: 1rem;
+    }
+
+    p {
+        line-height: 1.5rem;
+    }
+`;
--- pages/gen24/index.tsx
+++ pages/gen24/index.tsx
@@ -1,94 +1,19 @@
-import { type Sketch } from "@p5-wrapper/react";
 import { ExternalLinkWithIcon } from "components/ExternalLink";
-import { LinkStyleUnderlined } from "components/LinkStyles";
-import { Link } from "gatsby";
-import ReactP5WrapperWithFade from "p5/ReactP5WrapperWithFade";
 import * as React from "react";
-import styled from "styled-components";
+import * as C from "./components";
 import { sketch } from "./sketch";
 
 export const Content: React.FC = () => {
     return (
-        <Content_>
-            <FirstFold>
-                <Banner />
-                <SketchContainer />
-            </FirstFold>
-            <RestOfTheContent />
-        </Content_>
-    );
-};
-
-const Content_ = styled.div`
-    display: flex;
-    flex-direction: column;
-    max-width: 900px;
-    margin: auto;
-`;
-
-const FirstFold = styled.div`
-    min-height: 98svh;
-
-    display: flex;
-    flex-direction: column;
-`;
-
-const Banner: React.FC = () => {
-    return (
-        <Banner_>
-            <BannerH />
-        </Banner_>
-    );
-};
-
-const Banner_ = styled.div`
-    height: 67px;
-`;
-
-const SketchContainer: React.FC = () => {
-    return (
-        <SketchContainer_>
-            <Sketch />
-        </SketchContainer_>
-    );
-};
-
-const SketchContainer_ = styled.div`
-    flex-grow: 1;
-
-    display: flex;
-    justify-content: center; /* horizontally */
-    align-items: center; /* vertically */
-`;
-
-const BannerH: React.FC = () => {
-    return <BannerH_>GEN 24</BannerH_>;
-};
-
-const BannerH_ = styled.h3`
-    @media (max-width: 900px) {
-        margin-inline: 1rem;
-    }
-    color: var(--mrmr-color-3);
-    font-weight: 300;
-`;
-
-const Sketch: React.FC = () => {
-    return <ReactP5WrapperWithFade sketch={sketch} />;
-};
-
-const RestOfTheContent: React.FC = () => {
-    return (
-        <LinkStyleUnderlined>
+        <C.Content sketch={sketch}>
             <Description />
-            <Footer />
-        </LinkStyleUnderlined>
+        </C.Content>
     );
 };
 
 const Description: React.FC = () => {
     return (
-        <Description_>
+        <C.Description>
             <p>
                 Genuary is an month-long online art fair (that's one way of
                 putting it!) that happens every year, in, you guessed it,
@@ -110,47 +35,6 @@ const Description: React.FC = () => {
                 to view a live version.*/}
             </p>
             <p>Have a great and inspiring 2024.</p>
-        </Description_>
+        </C.Description>
     );
 };
-
-const Description_ = styled.div`
-    margin-block: 1rem;
-    @media (min-width: 800px) {
-        margin-block: 2rem;
-    }
-    @media (max-width: 900px) {
-        margin-inline: 1rem;
-    }
-
-    max-width: 30rem;
-
-    p {
-        line-height: 1.5rem;
-    }
-
-    a {
-        font-weight: normal;
-    }
-`;
-
-const Footer: React.FC = () => {
-    return (
-        <Footer_>
-            <p>
-                <Link to="/">mrmr.io</Link>
-            </p>
-        </Footer_>
-    );
-};
-
-const Footer_ = styled.div`
-    margin-block: 3rem;
-    @media (max-width: 900px) {
-        margin-inline: 1rem;
-    }
-
-    p {
-        line-height: 1.5rem;
-    }
-`;
