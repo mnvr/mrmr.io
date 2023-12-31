52d90168a168e6473f577674f8c750ba0735e167
Manav <manav@mrmr.io>
2023-12-31 15:32:08 +0530

    Sample sentence from a font picker

--- pages/quotes/quotes.ts
+++ pages/quotes/quotes.ts
@@ -73,5 +73,6 @@ export const quotes: string[] = [
     "Existence over essence",
     "Whose Justice? Which Rationality?",
     "Do something again and again from morning till evening, and to dream of it at night, and to think of nothing else than doing this well, as well as I alone can. When one lives this way, one thing after another that does not belong to such a life drops off, without hate or reluctance. What we do should determine what we forego.",
-    "Never approach stories one at a time. If one writes a story at time, one can end up writing the same story until one dies. It is best to write them three, or five at a time. More if you have the energy. Don't write two at a time either: their interplay, like lovers' mirrors,  produces melancholy."
+    "Never approach stories one at a time. If one writes a story at time, one can end up writing the same story until one dies. It is best to write them three, or five at a time. More if you have the energy. Don't write two at a time either: their interplay, like lovers' mirrors,  produces melancholy.",
+    "Sphinx of black quartz, judge my vow"
 ];
