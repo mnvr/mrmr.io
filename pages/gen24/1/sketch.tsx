c97b3e6441c3640dfbf450f0591e334bf67080dc
Manav <manav@mrmr.io>
2024-01-01 11:31:36 +0530

    Numbers

--- pages/gen24/1/sketch.tsx
+++ pages/gen24/1/sketch.tsx
@@ -1,3 +1,13 @@
-import { gridSketch } from "../grid";
+import { type CellShader, gridSketch } from "../grid";
 
-export const sketch = gridSketch();
+const drawCell: CellShader = ({p5, x, y}) => {
+    const t = p5.millis() / 1000 / 40;
+    const v = p5.floor(p5.abs(p5.sin(x**2 + y**2 + t)) * 100);
+    p5.textFont("monospace");
+    p5.textSize(16);
+    p5.text(`${v}`, x, y);
+};
+
+export const sketch = gridSketch({
+    drawCell,
+});
