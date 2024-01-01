a81b14575dfaa7ceb1ec006fe539d81ed7ddbf31
Manav <manav@mrmr.io>
2024-01-01 11:47:51 +0530

    Conditionally draw connectors

--- pages/gen24/1/sketch.tsx
+++ pages/gen24/1/sketch.tsx
@@ -1,6 +1,8 @@
 import { P5CanvasInstance } from "@p5-wrapper/react";
 import { gridSketch, type CellShader } from "../grid";
 
+const debug = false;
+
 /**
  * An arbitrary, smoothly varying value between 0 and 100 ([0, 100)) for the
  * cell at the given x and y coordinate.
@@ -9,26 +11,33 @@ import { gridSketch, type CellShader } from "../grid";
  * time.
  */
 const cellV = (p5: P5CanvasInstance, x: number, y: number) => {
-    const t = p5.millis() / 1000 / 40;
+    const t = p5.millis() / 1000 / 400;
     return p5.floor(p5.abs(p5.sin(x ** 2 + y ** 2 + t)) * 100);
 };
 
 const drawCell: CellShader = ({ p5, x, y, s }) => {
     const m = s / 2;
     const v = cellV(p5, x, y);
-    const vp = cellV(p5, x - 1, y);
-    p5.textFont("monospace");
-    p5.textSize(16);
-    p5.text(`${v}`, x, y);
 
+    if (debug) {
+        p5.textFont("monospace");
+        p5.textSize(16);
+        p5.text(`${v}`, x, y);
+    }
 
-    p5.line(x, y + m, x + m, y + m)
-    p5.line(x + m, y + m, x + s, y + m)
-    p5.line(x + m, y, x + m, y + m)
-    p5.line(x + m, y + m, x + m, y + s)
+    const vl = cellV(p5, x - 1, y);
+    const vr = cellV(p5, x + 1, y);
+    const vu = cellV(p5, x, y - 1);
+    const vd = cellV(p5, x, y + 1);
+
+    const th = 15; /* threshold */
+    if (p5.abs(v - vl) < th) p5.line(x, y + m, x + m, y + m);
+    if (p5.abs(v - vr) < th) p5.line(x + m, y + m, x + s, y + m);
+    if (p5.abs(v - vu) < th) p5.line(x + m, y, x + m, y + m);
+    if (p5.abs(v - vd) < th) p5.line(x + m, y + m, x + m, y + s);
 };
 
 export const sketch = gridSketch({
     drawCell,
-    showGuides: true,
+    showGuides: debug,
 });
