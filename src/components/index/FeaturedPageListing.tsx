cb2443715046dfed1edcaa7d2e52b60eae552428
Manav <manav@mrmr.io>
2024-01-03 21:31:36 +0530

    Fix the flashing of the featured page background images

--- src/components/index/FeaturedPageListing.tsx
+++ src/components/index/FeaturedPageListing.tsx
@@ -1,10 +1,5 @@
 import { Link } from "gatsby";
-import {
-    GatsbyImage,
-    getImage,
-    getSrc,
-    type ImageDataLike,
-} from "gatsby-plugin-image";
+import { GatsbyImage, getImage, type ImageDataLike } from "gatsby-plugin-image";
 import { ColorPalette } from "parsers/colors";
 import * as React from "react";
 import styled, { createGlobalStyle } from "styled-components";
@@ -40,62 +35,21 @@ export const FeaturedPageListing: React.FC<FeaturedPageListingProps> = ({
                     onMouseEnter={() => setHoverPage(page)}
                     onMouseLeave={() => setHoverPage(undefined)}
                 >
-                    <PageItemBackground
-                        $backgroundColor={page.colors?.backgroundColor1Transparent}
-                        $backgroundImage={
-                            page.previewImage
-                                ? getSrc(page.previewImage)
-                                : undefined
-                        }
+                    <PageItem
+                        $backgroundColor={page.colors?.backgroundColor1}
+                        $color={page.colors?.color1}
                     >
-                        <PageItem
-                            $backgroundColor={page.colors?.backgroundColor1Transparent}
-                            $backgroundImage={
-                                page.previewImage
-                                    ? getSrc(page.previewImage)
-                                    : undefined
-                            }
-                            $color={page.colors?.color1}
-                        >
+                        <BackgroundImage page={page} />
+                        <PageItemContent>
                             <PageItemP>{page.title.toLowerCase()}</PageItemP>
                             <PageItemCount>{n - i}</PageItemCount>
-                            {/* <BackgroundImage page={page} /> */}
-                        </PageItem>
-                    </PageItemBackground>
+                        </PageItemContent>
+                    </PageItem>
                 </Link>
             ))}
         </PageGrid>
     );
 };
-
-const BackgroundImage: React.FC<{ page: FeaturedPage }> = ({ page }) => {
-    const previewImage = page.previewImage;
-    const image = previewImage ? getImage(previewImage) : undefined;
-
-    // Use an empty alt since this is a decorative background image
-    return (
-        image && (
-            <BackgroundImageContainer>
-                <GatsbyImage image={image} alt="" />
-            </BackgroundImageContainer>
-        )
-    );
-};
-
-const BackgroundImageContainer = styled.div`
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-
-    /* Chrome 119 on macOS (and possibly others) still require the -webkit
-       vendor prefix for both mask-image and linear-gradient */
-    -webkit-mask-image: -webkit-linear-gradient(
-        transparent,
-        rgba(0, 0, 0, 0.3)
-    );
-    mask-image: linear-gradient(transparent, rgba(0, 0, 0, 0.3));
-`;
 
 /** A CSS transition that makes the background color changes more pleasing */
 export const BodyBackgroundColorTransitionStyle = createGlobalStyle`
@@ -129,43 +83,55 @@ const PageGrid = styled.div`
     }
 `;
 
-interface PageItemBackgroundProps {
-    $backgroundColor?: string;
-    $backgroundImage?: string;
-}
-
-const PageItemBackground = styled.div<PageItemBackgroundProps>`
-    /* opacity: 0.8; */
-    background-image: linear-gradient(
-            ${(props) => props.$backgroundColor ?? "inherit"},
-            transparent
-        ),
-        /* linear-gradient(to right, rgba(255,255,255, 0.7) 0 100%), */
-        url(${(props) => props.$backgroundImage});
-
-    p {
-        opacity: 1 !important;
-    }
-`;
-
 interface PageItemProps {
     $backgroundColor?: string;
-    $backgroundImage?: string;
     $color?: string;
 }
 
 const PageItem = styled.div<PageItemProps>`
-    /* background-color: ${(props) => props.$backgroundColor ?? "inherit"}; */
-    /* background-image: linear-gradient(
-            ${(props) => props.$backgroundColor ?? "inherit"},
-            transparent
-        ),
-        url(${(props) => props.$backgroundImage}); */
+    background-color: ${(props) => props.$backgroundColor ?? "inherit"};
     color: ${(props) => props.$color ?? "inherit"};
     width: 13.7ch;
     height: 9.7ch;
+
+    display: grid;
+`;
+
+const BackgroundImage: React.FC<{ page: FeaturedPage }> = ({ page }) => {
+    const previewImage = page.previewImage;
+    const image = previewImage ? getImage(previewImage) : undefined;
+
+    // Use an empty alt since this is a decorative background image
+    return (
+        <BackgroundImageContainer>
+            {image && <GatsbyImage image={image} alt="" />}
+        </BackgroundImageContainer>
+    );
+};
+
+const BackgroundImageContainer = styled.div`
+    grid-area: 1 / 1;
+
+    /* Same size as parent */
+    width: 13.7ch;
+    height: 9.7ch;
+
+    /* Chrome 119 on macOS (and possibly others) still require the -webkit
+       vendor prefix for both mask-image and linear-gradient */
+    -webkit-mask-image: -webkit-linear-gradient(
+        transparent,
+        rgba(0, 0, 0, 0.3)
+    );
+    mask-image: linear-gradient(transparent, rgba(0, 0, 0, 0.3));
+`;
+
+const PageItemContent = styled.div`
     padding-block: 0.33rem;
     padding-inline: 0.66rem;
+
+    grid-area: 1 / 1;
+    z-index: 1;
+
     position: relative;
 `;
 
