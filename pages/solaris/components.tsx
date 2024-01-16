dbcf662c22e4fe24563755b19936b4b5fa666309
Manav <manav@mrmr.io>
2024-01-16 18:44:07 +0530

    Edit and lint

--- pages/solaris/components.tsx
+++ pages/solaris/components.tsx
@@ -1,10 +1,70 @@
 import * as React from "react";
 import styled from "styled-components";
 
-export const Indent: React.FC = () => {
-    return <Indent_> </Indent_>;
+export const Poem: React.FC = () => {
+    return (
+        <section>
+            <p>
+                <i>Solaris</i>
+                <br />
+                <Line>Can't touch me</Line>
+                <Line>Can hit me</Line>
+                <Line>Can't hurt me</Line>
+            </p>
+            <p>
+                Solaris
+                <br />
+                <Line>Meet Mimir</Line>
+                <Line>Another dawn</Line>
+                <Line>We watch</Line>
+                <Line>Moving on</Line>
+            </p>
+            <p>
+                Solaris
+                <br />
+                <Line>Why the distaste</Line>
+                <Line>Why the hate</Line>
+                <Line>They are what they are</Line>
+            </p>
+            <p>
+                Solaris
+                <br />
+                <Line>They can't be anything else</Line>
+                <Line>They didn't choose to be</Line>
+                <Line>Blind brutal eternity</Line>
+            </p>
+            <p>
+                Solaris
+                <br />
+                <Line>Drenched in your light</Line>
+                <Line>I feel everything is all right</Line>
+            </p>
+            <p>
+                Solaris
+                <br />
+                <Line>Time breaks me down</Line>
+                <Line>With time I'll cease to be</Line>
+                <Line>Yet</Line>
+                <Line>Time is my testimony</Line>
+            </p>
+            <p>
+                <i>Solaris</i>
+            </p>
+            <br />
+        </section>
+    );
+};
+const Line: React.FC<React.PropsWithChildren> = ({ children }) => {
+    return (
+        <Line_>
+            <>
+                {children}
+                <br />
+            </>
+        </Line_>
+    );
 };
 
-const Indent_ = styled.span`
+const Line_ = styled.span`
     padding-inline: 0.7rem;
 `;
