f614db61f9311509c02ce8733cf1cdd70beb8e9b
Manav <manav@mrmr.io>
2024-01-18 12:05:56 +0530

    Title

--- pages/raga/bhairav/index.tsx
+++ pages/raga/bhairav/index.tsx
@@ -1,5 +1,22 @@
+import { WideColumn } from "components/Column";
 import * as React from "react";
+import styled from "styled-components";
 
 export const Content: React.FC = () => {
-    return <h1>raga bhairav</h1>;
+    return (
+        <WideColumn>
+            <Title>
+                <T1>raga</T1> bhairav
+            </Title>
+        </WideColumn>
+    );
 };
+
+const Title = styled.h1`
+    font-family: serif;
+    font-style: italic;
+`;
+
+const T1 = styled.span`
+    color: var(--mrmr-color-4);
+`;
