3e5f682101177bb735bf3d848a1f4b8fe8f3facf
Manav <manav@mrmr.io>
2024-01-20 14:01:11 +0530

    Another gain

--- pages/raag/bhairav/synth.ts
+++ pages/raag/bhairav/synth.ts
@@ -57,7 +57,10 @@ export class Synth {
      * @param params The {@link PlayParams}
      */
     async play(params?: PlayParams) {
-        const { midiNote } = params ?? {};
+        const { midiNote, gain } = validateParams({
+            ...params,
+            ...defaultPlayParams,
+        });
 
         const ctx = this.init();
 
@@ -76,13 +79,36 @@ export class Synth {
         const t = ctx.currentTime;
 
         /** Play a sine tone at 440 Hz for 0.125 seconds */
-        const osc = ctx.createOscillator();
-        osc.type = "sine";
-        osc.frequency.setValueAtTime(440, t);
-        osc.connect(ctx.destination);
+        const osc = new OscillatorNode(ctx, {
+            type: "sine",
+            frequency: 440,
+        });
+
+        const oscOut = new GainNode(ctx, {
+            gain,
+        });
+        osc.connect(oscOut);
+
+        /**
+         * Apply a relatively strong attentuation to the output always, to avoid
+         * accidentally emitting loud noises, both during development, and for
+         * people who might have their speakers unknowingly turned on too loud.
+         */
+        const out = new GainNode(ctx, {
+            gain: 0.3,
+        });
+        out.connect(ctx.destination);
+
+        oscOut.connect(out);
         osc.start();
     }
 }
+
+/** A unipolar level value, between 0 and 1 (inclusive) */
+type Level = number;
+
+/** A bipolar value between -1 and 1 (inclusive) */
+type Bipolar = number;
 
 interface PlayParams {
     /**
@@ -96,4 +122,45 @@ interface PlayParams {
      * @default 69.
      */
     midiNote?: number;
+    /**
+     * Multiplier for the level of the output.
+     *
+     * Note that as a safety measure, there is a hardcoded attenuation (gain of
+     * 0.3) applied to the final signal before sending it to the destination.
+     * The gain specified here is independent of that.
+     *
+     * @default 0.3.
+     */
+    gain: Level;
 }
+
+const defaultPlayParams: Required<PlayParams> = {
+    midiNote: 69,
+    gain: 0.3,
+};
+
+/**
+ * We use type aliases to annotate some of the parameters, e.g. the type of the
+ * {@link gain} param is {@link Level}.
+ *
+ * However, these are just type aliases, and bring no runtime safety. For that,
+ * we pass the parameters through this function that raises an exception if any
+ * of the values passed don't quite look right.
+ *
+ * @return the passed in parameters.
+ */
+const validateParams = (params: Required<PlayParams>) => {
+    const { midiNote, gain } = params;
+
+    ensureLevel(gain);
+
+    return params;
+};
+
+/** Throw if the given value does not look like a {@link Level} */
+const ensureLevel = (v: Level) => {
+    if (v < 0 || v > 1)
+        throw new Error(
+            `Invalid level ${v}. Level values are expected to be in the range 0 and 1 (inclusive).`,
+        );
+};
