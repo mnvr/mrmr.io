a5dd9b10a03d695559337832117f147cad58231b
Manav <manav@mrmr.io>
2024-01-23 15:10:01 +0530

    Card 1

--- pages/raag/index.tsx
+++ pages/raag/index.tsx
@@ -7,7 +7,7 @@ export const Contents: React.FC = ({}) => {
         <Container_>
             <WideColumn>
                 <Title />
-                Hello
+                <RaagList />
             </WideColumn>
         </Container_>
     );
@@ -55,4 +55,93 @@ const T1 = styled.span`
 
 const T1H = styled.span`
     color: var(--mrmr-color-3);
+`;
+
+const RaagList: React.FC = () => {
+    return (
+        <RaagList_>
+            <li>
+                <RaagBhairav />
+            </li>
+        </RaagList_>
+    );
+};
+
+const RaagList_ = styled.ul`
+    list-style: none;
+    padding-inline-start: 0;
+
+    li {
+        margin: 1em;
+    }
+`;
+
+const RaagBhairav: React.FC = () => {
+    return (
+        <RaagBhairav_>
+            <RaagName>
+                <div>raag</div> <div className="highlighted">bhairav</div>
+            </RaagName>
+            <Notes />
+        </RaagBhairav_>
+    );
+};
+
+const RaagName = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.4em;
+`;
+
+const RaagBhairav_ = styled.div`
+    background-color: oklch(49.35% 0.025 53.84);
+    color: oklch(84.24% 0.006 43.32);
+    --mrmr-note-highlighted-color: oklch(98% 0 0);
+
+    @media (prefers-color-scheme: dark) {
+        background-color: oklch(25% 0.05 83.84);
+        color: oklch(94% 0 0);
+        --mrmr-note-muted-color: oklch(75.15% 0.013 86.85);
+    }
+
+    border-radius: 3px;
+
+    padding-inline: 1.5em;
+    padding-block: 1.25em;
+    font-style: italic;
+    font-family: serif;
+
+    display: flex;
+    justify-content: space-between;
+
+    .highlighted {
+        color: var(--mrmr-note-highlighted-color);
+    }
+`;
+
+const Notes: React.FC = () => {
+    const notes = [0, 1, 4, 5, 7, 8, 11];
+    return (
+        <Notes_>
+            {Array(12)
+                .fill(0)
+                .map((_, i) => (
+                    <div key={i}>{i}</div>
+                ))}
+        </Notes_>
+    );
+};
+
+const Notes_ = styled.div`
+    border: 1px solid tomato;
+    display: flex;
+    gap: 12px;
+
+    & > div {
+        width: 15px;
+        height: 100%;
+        border-radius: 3px;
+        background-color: oklch(84.24% 0.006 43.32 / 0.6);
+    }
+
 `;
