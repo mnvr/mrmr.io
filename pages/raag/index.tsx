87f6c523518ebb0302f38ecd5558e59809dd29f9
Manav <manav@mrmr.io>
2024-01-23 16:13:39 +0530

    Cleanup

--- pages/raag/index.tsx
+++ pages/raag/index.tsx
@@ -21,10 +21,10 @@ const Title: React.FC = () => {
     return (
         <Title_>
             <h1>
-                raag <T1>city</T1>
+                raag <span className="muted">city</span>
             </h1>
             <big>
-                रागों <T1H>की नगरी</T1H>
+                रागों <span className="muted">की नगरी</span>
             </big>
         </Title_>
     );
@@ -41,20 +41,20 @@ const Title_ = styled.div`
     h1 {
         font-size: 3em;
         margin-block-end: 7px;
+
+        .muted {
+            color: var(--mrmr-color-3);
+            margin-inline-start: -5px;
+        }
     }
 
     big {
         font-size: 1.6em;
+
+        .muted {
+            color: var(--mrmr-color-3);
+        }
     }
-`;
-
-const T1 = styled.span`
-    color: var(--mrmr-color-3);
-    margin-inline-start: -5px;
-`;
-
-const T1H = styled.span`
-    color: var(--mrmr-color-3);
 `;
 
 const RaagList: React.FC = () => {
@@ -79,75 +79,66 @@ const RaagList_ = styled.ul`
 const RaagBhairav: React.FC = () => {
     return (
         <RaagBhairav_>
-            <RaagName>
-                <div>bhairav</div>
-            </RaagName>
+            bhairav
             <Notes />
         </RaagBhairav_>
     );
 };
 
-const RaagName = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 0.4em;
-    min-height: 50px;
-`;
-
 const RaagBhairav_ = styled.div`
+    /* Taken from the page colors */
     background-color: oklch(49.35% 0.025 53.84);
-    color: oklch(84.24% 0.006 43.32);
-    --mrmr-note-highlighted-color: oklch(98% 0 0);
+    color: oklch(84.24% 0.006 43.32 / 0.8);
+    --mrmr-raag-card-muted-color: oklch(84.24% 0.006 43.32 / 0.5);
+    --mrmr-raag-card-highlighted-color: oklch(98% 0 0);
 
     @media (prefers-color-scheme: dark) {
         background-color: oklch(25% 0.05 83.84);
-        color: oklch(94% 0 0);
-        --mrmr-note-muted-color: oklch(75.15% 0.013 86.85);
+        color: oklch(75.15% 0.013 86.85 / 0.6);
+        --mrmr-raag-card-muted-color: oklch(75.15% 0.013 86.85 / 0.5);
+        --mrmr-raag-card-highlighted-color: oklch(94% 0 0);
     }
-
-    border-radius: 3px;
 
     padding-inline: 1.5em;
     padding-block: 1.25em;
-    /* font-style: italic; */
-    /* font-family: monospace; */
+    border-radius: 3px;
+
     font-variant: small-caps;
 
     display: flex;
     justify-content: space-between;
 
+    .muted {
+        background-color: var(--mrmr-raag-card-muted-color);
+    }
+
     .highlighted {
-        color: var(--mrmr-note-highlighted-color);
-        background-color: var(--mrmr-note-highlighted-color);
+        background-color: var(--mrmr-raag-card-highlighted-color);
     }
 `;
 
 const Notes: React.FC = () => {
     const notes = [0, 1, 4, 5, 7, 8, 11];
+    const seq = () => Array(12).fill(0);
     return (
         <Notes_>
-            {Array(12)
-                .fill(0)
-                .map((_, i) => (
-                    <div
-                        key={i}
-                        className={notes.includes(i) ? "highlighted" : ""}
-                    />
-                ))}
+            {seq().map((_, i) => (
+                <div
+                    key={i}
+                    className={notes.includes(i) ? "highlighted" : "muted"}
+                />
+            ))}
         </Notes_>
     );
 };
 
 const Notes_ = styled.div`
-    /* border: 1px solid tomato; */
     display: flex;
     gap: 12px;
 
     & > div {
-        width: 15px;
+        width: 12px;
         height: 100%;
         border-radius: 3px;
-        background-color: oklch(84.24% 0.006 43.32 / 0.6);
     }
 `;
