2100365652b634a0b10bd729e6cc3feeb82981d3
Manav <manav@mrmr.io>
2024-01-24 19:29:20 +0530

    Go back to symmetry

--- pages/gen24/25/sketch.tsx
+++ pages/gen24/25/sketch.tsx
@@ -32,62 +32,60 @@ const drawCell0: CellShader<State> = ({ p5, x, y, s }) => {
 
 type P = [number, number];
 
-const drawCell: CellShader<State> = ({ p5, x, y, s, cell, state }) => {
-    // Move the top anchor point 1 towards the left
-    const r1 = 4; // [1, 4]
-    // Offset c1 from the center. Positive values move it leftwards.
-    const r2 = 4; // [4, -4]
-    // Offset c2 downward from its standard position (1/8 of s from the top).
-    const r3 = 4; // [-4, 4]
-    // Offset the left anchor point from its standard position (1/8 of s from
-    // the left). Positive values move it leftwards.
-    const r4 = 4; // [-4, 4]
-    // Offset c3 downwards from its standard position (1/8 of s from the
-    // bottom). Positive values move it upwards.
-    const r5 = 4; // [-4, 4]
-    // Offset c4 from the center. Positive values move it leftwards.
-    const r6 = 4; // [-4, 4]
-    // Move the bottom anchor point 1 towards the left.
-    const r7 = 4; // [1, 4]
+/**
+ * Return a new point that is the result of introducing a random jitter to the
+ * given point.
+ *
+ * @param pt The point to jitter.
+ * @param onlyHoriz If true, there is no vertical jitter added.
+ */
+const jiggle = (pt: P, onlyHoriz = false): P => {
+    const jx = Math.floor(Math.random() * 4);
+    const jy = onlyHoriz ? 0 : Math.floor(Math.random() * 4);
+    return [pt[0] + jx, pt[1] + jy];
+};
 
+const drawCell: CellShader<State> = ({ p5, x, y, s, cell, state }) => {
     // Draw four bezier curves, roughly approximating a diamond horizontally
     // centered in the cell and running along its entire height. The anchor
     // points (a*) and the control points (c*) below are chosen so as to give a
     // bit of a bulge to the "bulby" shape that we draw.
 
     // Top anchor point 1
-    const a1: P = [x + s / 2 - r1, y];
+    const a1: P = [x + s / 2, y];
     // Control points
-    const c1: P = [x + s / 2 - r2, y + s / 2 - s / 4];
-    const c2: P = [x + s / 8, y + s / 8 + r3];
+    const c1: P = [x + s / 2, y + s / 2 - s / 4];
+    const c2: P = [x + s / 8, y + s / 8];
 
     // Left anchor point
-    const a2: P = [x + s / 8 - r4, y + s / 2];
+    const a2: P = [x + s / 8, y + s / 2];
 
-    // Inexact mirror of c2
-    const c3: P = [x + s / 8, y + s - s / 8 - r5];
+    // Almost mirror of c2
+    const c3: P = [x + s / 8, y + s - s / 8];
     // Similar to c1
-    const c4: P = [x + s / 2 - r6, y + s - s / 4];
+    const c4: P = [x + s / 2, y + s - s / 4];
 
     // Bottom anchor point 1
-    const a3: P = [x + s / 2 - r7, y + s];
+    const a3: P = [x + s / 2, y + s];
 
     // Now let's draw the other side.
 
     // Bottom anchor point 2
     const a4: P = [x + s / 2, y + s];
-    // Right anchor point, an inexact mirror of the left one.
-    const a5: P = [x + s - s / 8, y + s / 2];
 
     // Control points, following a similar pattern as the left side.
     // Inexact mirror of c4
-    const c5: P = [x + s / 2, y + s / 2];
+    const c5: P = [x + s / 2, y + s - s / 4];
     // Inexact mirror of c3
-    const c6: P = [x + s - s / 8, y + s / 2 + s / 4];
-    // Mirror of c6
-    const c7: P = [x + s - s / 8, y + s / 2 - s / 4];
+    const c6: P = [x + s - s / 8, y + s - s / 8];
+
+    // Right anchor point, an inexact mirror of the left one.
+    const a5: P = [x + s - s / 8, y + s / 2];
+
+    // Almost mirror of c6
+    const c7: P = [x + s - s / 8, y + s / 8];
     // Inexact mirror of c1
-    const c8: P = [x + s / 2, y + s / 2];
+    const c8: P = [x + s / 2, y + s / 2 - s / 4];
 
     // Top anchor point 2
     const a6: P = [x + s / 2, y];
@@ -107,12 +105,18 @@ const drawCell: CellShader<State> = ({ p5, x, y, s, cell, state }) => {
     p5.circle(...c2, 5);
     p5.circle(...c3, 5);
     p5.circle(...c4, 5);
+    p5.circle(...c5, 5);
+    p5.circle(...c6, 5);
+    p5.circle(...c7, 5);
+    p5.circle(...c8, 5);
 
     p5.fill("green");
     p5.circle(...a1, 5);
     p5.circle(...a2, 5);
     p5.circle(...a3, 5);
     p5.circle(...a4, 5);
+    p5.circle(...a5, 5);
+    p5.circle(...a6, 5);
     p5.pop();
 };
 
