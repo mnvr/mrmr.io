2c91b75ff0be4376aaa743ab48c742fa96511e5c
Manav <manav@mrmr.io>
2024-01-25 20:46:46 +0530

    Move to JS

---
+++ pages/mj/components.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import styled from "styled-components";
+
+export const Container: React.FC<React.PropsWithChildren> = ({ children }) => {
+    return <Container_>{children}</Container_>;
+};
+
+const Container_ = styled.div`
+    margin: 1em;
+`;
