cbeedbbbe9d38fa93e960d0c53a851082cdd4158
Manav <manav@mrmr.io>
2024-01-28 16:43:35 +0530

    Tweak Solaris

--- pages/solaris/components.tsx
+++ pages/solaris/components.tsx
@@ -5,58 +5,54 @@ export const Poem: React.FC = () => {
     return (
         <section>
             <p>
-                <i>Solaris</i>
-                <br />
+                <i>
+                    <Solaris />
+                </i>
                 <Line>Can't touch me</Line>
                 <Line>Can hit me</Line>
                 <Line>Can't hurt me</Line>
             </p>
             <p>
-                Solaris
-                <br />
+                <Solaris />
                 <Line>Meet Mimir</Line>
                 <Line>Another dawn</Line>
                 <Line>We watch</Line>
                 <Line>Moving on</Line>
             </p>
             <p>
-                Solaris
-                <br />
+                <Solaris />
                 <Line>Why the distaste</Line>
                 <Line>Why the hate</Line>
                 <Line>They are what they are</Line>
             </p>
             <p>
-                Solaris
-                <br />
+                <Solaris />
                 <Line>They didn't choose to be</Line>
                 <Line>Blind brutal</Line>
                 <Line>Eternity</Line>
             </p>
             <p>
-                Solaris
-                <br />
+                <Solaris />
                 <Line>Drenched in your light</Line>
                 <Line>Everything is all right</Line>
             </p>
             <p>
-                Solaris
-                <br />
+                <Solaris />
                 <Line>Time breaks me down</Line>
                 <Line>In time I cease to be, yet</Line>
                 <Line>Time is my testimony</Line>
             </p>
             <p>
-                Solaris
-                <br />
+                <Solaris />
                 <Line>Cold dark mountain</Line>
                 <Line>Making me bleed</Line>
                 <Line>Float, and be free</Line>
             </p>
             <p>
-                <i>Solaris</i>
+                <i>
+                    <Solaris />
+                </i>
             </p>
-            <br />
         </section>
     );
 };
@@ -65,12 +61,16 @@ const Line: React.FC<React.PropsWithChildren> = ({ children }) => {
         <Line_>
             <>
                 {children}
-                <br />
+                <>{" – "}</>
             </>
         </Line_>
     );
 };
 
 const Line_ = styled.span`
-    padding-inline: 0.7rem;
+    padding-inline-start: 2px;
 `;
+
+const Solaris: React.FC = () => {
+    return <div>Solaris</div>;
+};
