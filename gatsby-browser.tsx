27348236c060ba238320e7d3624631ea37f42a49
Manav <manav@mrmr.io>
2024-01-29 12:41:52 +0530

    Use the same monospace font across browsers
    The default monospace on Chrome and Safari are quite different, and for the same
    font-size they look quite different. This is a problem that effects our site in
    general too, the default system-ui is, as the name suggests, system dependent,
    but with monospace fonts we have the luxury that they're usually not in the
    first fold, so we can use a network request with less concern about amateur
    looking swaps.
    
    With a fixed fixed-width font (do I like repeating words, maybe), we no longer
    have to pick a font-size that works for everything and nothing, and can use a
    size that should look the same everywhere.
    
    Steps:
    
    1. yarn add @fontsource/fira-mono
    2. Add import '@fontsource/fira-mono' to gatsby-browser
    3. Set that as the default code font

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,5 +1,16 @@
 import type { GatsbyBrowser } from "gatsby";
 
+// This enables us to use
+//
+//     body {
+//         font-family: 'Fira Mono', monospace;
+//     }
+//
+// References:
+// - https://fontsource.org/fonts/fira-mono/install
+// - https://www.gatsbyjs.com/docs/how-to/styling/using-web-fonts/
+import "@fontsource/fira-mono";
+
 // These need to be a relative paths (similar to how we need to use relative
 // paths in `gatsby-node.ts`).
 import { wrapPageElementImpl } from "./src/gatsby/wrap";
