68ab368fafab8b9e549e71719cbf54af6650aad0
Manav <manav@mrmr.io>
2024-01-30 11:46:33 +0530

    Include code

--- pages/mj/euclid/test.js
+++ pages/mj/euclid/test.js
@@ -65,6 +65,30 @@ const E = (k, n) => {
     return seqs.flat();
 };
 
+/** The tinier variant that we show in the tutorial text */
+// prettier-ignore
+const Et = (k, n) => {
+    let s = Array(n).fill(0)
+        .map((_, i) => (i < k ? [1] : [0]))
+
+    let d = n - k
+    n = Math.max(k, d)
+    k = Math.min(k, d)
+    let z = d
+
+    while (z > 0 || k > 1) {
+        for (let i = 0; i < k; i++)
+            s[i].push(...s[s.length - 1 - i])
+        s.splice(-k)
+        z = z - k
+        d = n - k
+        n = Math.max(k, d)
+        k = Math.min(k, d)
+    }
+
+    return s.flat()
+};
+
 const test = (k, n, expected) => {
     const seq = JSON.stringify(E(k, n));
     const exp = JSON.stringify(expected);
