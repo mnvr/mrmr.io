28fbbf850699c3edbad5bad2bea2d692b8c90c15
Manav <manav@mrmr.io>
2024-01-30 09:38:05 +0530

    Rewrite, this time trying to verbatim follow the paper

--- pages/mj/euclid/test.js
+++ pages/mj/euclid/test.js
@@ -1,116 +1,65 @@
 /**
- * A JavaScript CLI program to generate Euclidean Rhythms
+ * An implementation of Bjorklund's algorithm for calculating the Euclidean
+ * rhythms, as described in the 2005 (extended) paper _The Euclidean //
+ * Algorithm Generates Traditional Musical Rhythms_ by Godfried Toussaint.
  *
- * This is meant as a test bed for our algorithm. It is in JavaScript so that it
- * can directly be invoked without needing transpilation:
+ * This is a standalone JavaScript program  meant as a test bed for our
+ * algorithm.  It is in JavaScript so that it can directly be invoked without
+ * needing transpilation:
  *
  *     node pages/mj/euclid/test.js
  *
  * It will then generate some Euclidean Rhythms, and compare them against the
- * examples given in Toussaint's paper, _The Euclidean Algorithm Generates
- * Traditional Musical Rhythms_.
+ * examples given in Toussaint's paper.
  */
 
 /**
  * Euclidean rhythm E(k, n) where k is the number of 1's and n is the length of
- * the sequence.
+ * the sequence. The property that such sequences have is that the 1's are
+ * maximally spaced out (in a fractal sense).
  */
 const E = (k, n) => {
-    const seq = Array(n)
+    const seqs = Array(n)
         .fill(0)
         .map((_, i) => (i < k ? [1] : [0]));
 
-    // const fold = (n, k, seq) => {
-    //     if (k <= 2) return seq;
-    //     const sm = Math.min(k, seq.length - k);
-    //     const leading = seq.slice(0, seq.length - sm);
-    //     const trailing = seq.slice(-1 * sm);
-    //     const llen = leading.length;
-    //     const tlen = trailing.length;
-
-    //     let result = [];
-    //     for (let i = 0; i < Math.max(llen, tlen); i++) {
-    //         let r = [];
-    //         if (i < llen) {
-    //             r.push(...leading[i]);
-    //         }
-    //         if (i < tlen) {
-    //             r.push(...trailing[i]);
-    //         }
-    //         result.push(r);
-    //     }
-
-    //     // console.log({ n, k, seq, leading, trailing, result });
-
-    //     return fold(k, n % k, result);
-    // };
-
     const debug = true;
 
-    // const fold = (n, k, z, seq) => {
-    //     if (k < 2) return seq;
+    const fold = (n, k, z, seqs) => {
+        let result = [...seqs];
 
-    //     let result = [...seq];
-    //     if (debug) console.log({ n, k, z, result });
-
-    //     // If k divides into n, fold the sequence symmetrically
-    //     if (n % k === 0) {
-    //         for (let i = 0; i < k; i++) {
-    //             for (let j = i + k; j < n; j += k) {
-    //                 result[i] = [...result[i], ...result[j]];
-    //             }
-    //         }
-    //         result = result.slice(0, k);
-    //     } else if (z > 0) {
-    //         // limit the fold to z (number of remaining zeros)
-    //         let i = 0;
-    //         for (; i < Math.min(k, z); i++) {
-    //             result[i] = [...result[i], ...result[result.length - i - 1]];
-    //             if (debug) console.log({ i, k, result });
-    //         }
-    //         result = result.slice(0, -i);
-    //     } else {
-    //         for (let i = 0; i < k; i++) {
-    //             result[i] = [...result[i], ...result[result.length - i - 1]];
-    //             if (debug) console.log({ i, k, result });
-    //         }
-    //         result = result.slice(0, -k);
-    //     }
-
-    //     if (debug) console.log("after slicing", result);
-    //     return fold(k, n % k, 0, result);
-    // };
-
-    // An implementation of Bjorklund's algorithm for calculating the Euclidean
-    // rhythms, as described in the 2005 (extended) paper _The Euclidean
-    // Algorithm Generates Traditional Musical Rhythms_ by Godfried Toussaint.
-    const fold = (n, k, z, seq) => {
-        let result = [...seq];
-
-        // Remainder
-        let rem = n;
-
+        // Description from the paper:
+        //
         // > If there is more than one zero the algorithm moves zeros in stages.
         //   We begin by taking zeroes one-at-a-time (from right to left),
         //   placing a zero after each one (from left to right).
-        let nz = z;
-        while (nz > 0) {
-            // Number of zeros to move in this stage is the minimum of the
-            // remaining zeros and the remainder (k).
-            const toMove = Math.min(nz, k);
-            for (let i = 0; i < toMove; i++) {
+        //
+        // The same stages happen but with the "remainder" right-most sequences
+        // after the zeros have been exhausted. Then,
+        //
+        // > The process stops when the remainder consists of only one sequence,
+        //   or we run out of zeros.
+        while (z > 0 || k > 1) {
+            const m = z > 0 && k > 1 ? Math.min(z, k) : z > 0 ? z : k;
+            for (let i = 0; i < m; i++) {
                 result[i] = [...result[i], ...result[result.length - 1 - i]];
             }
-            result = result.slice(0, result.length - toMove);
-            nz = nz - toMove;
-            rem = rem - toMove;
-            if (debug) console.log(`stage: after moving ${toMove} zeroes`, {nz, rem, result});
+            result = result.slice(0, result.length - m);
+            if (debug) console.log(`moved ${m}`, { n, k, z, result });
+            const r = n - k;
+            n = Math.max(k, r);
+            k = Math.min(k, r);
+            z = z - m;
+            if (debug) console.log({ r, n, k, z });
         }
 
         return result;
     };
 
-    return fold(n, k, n - k, seq).flat();
+    const r = n - k;
+    n = Math.max(k, r);
+    k = Math.min(k, r);
+    return fold(n, k, r, seqs).flat();
 };
 
 const test = (k, n, expected) => {
@@ -120,7 +69,6 @@ const test = (k, n, expected) => {
     if (seq != exp) {
         console.log(`Expectd\t${exp}`);
         console.assert(seq == exp, `Expected E(${k}, ${n}) to match ${exp}`);
-        process.exit(1);
     }
 };
 
@@ -131,15 +79,12 @@ const test = (k, n, expected) => {
 // ---
 
 // Regular / periodic / "isochronous" rhythms
-//
-// The paper calls them out separately; our algorithm too has a special case for
-// dealing with them.
 // test(4, 16, [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0]);
 // test(3, 12, [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0]);
 
 // // The three main examples from the paper illustrating the algorithm
 test(5, 13, [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0]);
-// test(3, 8, [1, 0, 0, 1, 0, 0, 1, 0]);
+test(3, 8, [1, 0, 0, 1, 0, 0, 1, 0]);
 // test(5, 8, [1, 0, 1, 1, 0, 1, 1, 0]);
 
 // // The paper lists [1, 0, 1], we get a rotated version
