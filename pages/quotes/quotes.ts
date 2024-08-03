56a0c5813cf469df9e60a1cf8edaf1d04e91ef78
Manav Rathi <manav@mrmr.io>
2024-08-03 19:53:37 +0530

    Zhuangzi
    https://mastodon.social/@pyroboyee@mstdn.social/109749856832122557

--- pages/quotes/quotes.ts
+++ pages/quotes/quotes.ts
@@ -80,4 +80,5 @@ export const quotes: string[] = [
     "Nothing new will be said here, nor have I any skill in composition. Therefore I do not imagine that I can benefit others. I have done this to perfume my own mind.",
     "Behind him, across vast distances of space and time, from the place he had left, he thought he heard music too. But perhaps it was only an echo.",
     "I always wanted to go to East Cliff, more years than I can remember, until today I just grabbed a vine and started up. Halfway up wind and a heavy mist closed in, and it was hard to get on. The slickery mud under the moss on the rocks gave way, and I couldn't keep going. So here I stay, under this cinnamon tree, white clouds for my pillow, I'll just take a nap.",
+    "I was conscious only of my happiness as a butterfly, but then I woke, veritably myself again. Now I don't know whether I was then a man dreaming I was a butterfly, or whether I am not a butterfly dreaming I am a man",
 ];
