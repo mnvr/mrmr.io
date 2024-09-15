62499129f3db3dd0f3652f4915208c2ced1b04bb
Manav Rathi <manav@mrmr.io>
2024-09-15 09:31:02 +0530

    inline

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import styled from "styled-components";
+import { sketch } from "../pages/universe-is-its-own-simulation/sketch";
+import ReactP5Wrapper from "./p5/ReactP5Wrapper";
 
 const App = () => {
-    return <div>Hello</div>;
+    return (
+        <div>
+            <UnivApp />
+        </div>
+    );
+    // return <div>Hello</div>;
 };
+
+const UnivApp = () => {
+    return <Sketch></Sketch>;
+};
+
+export const Sketch: React.FC = () => {
+    return (
+        <Sketch_>
+            <ReactP5Wrapper sketch={sketch} />
+        </Sketch_>
+    );
+};
+
+const Sketch_ = styled.div`
+    /** Provide a minimum height to prevent a layout shift on load */
+    min-height: 100vh;
+    margin-bottom: 4rem;
+`;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
--- vite.config.mts
+++ vite.config.mts
@@ -1,7 +1,22 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+const mrmr = () => {
+    return {
+        name: "mrmr",
+
+        resolveId(id) {
+            console.log(arguments);
+            if (id.startsWith("utils/")) {
+                return (import.meta.dirname + "/src/" + id + ".ts");
+            }
+        },
+    };
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), mrmr()],
+    // TODO
+    publicDir: "static",
 });
