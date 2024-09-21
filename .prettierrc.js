21664ec4178dbbeec4065993db3093d29cfcc8e8
Manav Rathi <manav@mrmr.io>
2024-09-21 19:55:26 +0530

    Try prettier defaults for HTML

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,12 @@
 module.exports = {
     tabWidth: 4,
     plugins: ["prettier-plugin-organize-imports"],
+    overrides: [
+        {
+            files: ["*.html"],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
 };
