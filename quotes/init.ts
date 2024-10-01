45a44be6358062d309a2a651c037ce85a73ce420
Manav Rathi <manav@mrmr.io>
2024-10-01 08:12:09 +0530

    The name conflicted

--- quotes/init.ts
+++ quotes/init.ts
@@ -4,21 +4,40 @@ import { ensure, ensureNumber } from "../src/utils/ensure";
 import { randomInt, randomItem } from "../src/utils/random";
 
 export const parsedQuotes = parseQuotes(quotes);
+console.log({ parsedQuotes });
 
-export const fontSizeForQuoteLength = (len: number) => {
-    if (len > 300) return "1.4rem";
-    if (len > 200) return "1.6rem";
-    if (len > 80) return "1.8rem";
-    return "2rem";
-};
-
-// The index of the quote that we are currently showing. It indexes into
-// {@link quotes} property of {@link parsedQuotes}.
-//
-// This will be undefined initially, when we haven't yet shown the initial
-// quote. The initial quote is randomly selected on page load.
+/**
+ * The index of the quote that we are currently showing. It indexes into
+ * {@link quotes} property of {@link parsedQuotes}.
+ *
+ * This will be undefined initially, when we haven't yet shown the initial
+ * quote. The initial quote is randomly selected on page load.
+ */
 
 let _quoteIndex: number | undefined;
+
+const init = () => {
+    // window.addEventListener("popstate", handlePopState);
+    loadInitialQuote();
+};
+
+const handlePopState = (event: PopStateEvent) => {
+    const { state } = event;
+    console.log("handlePopState", state);
+
+    // When the user presses back from this page itself the
+    // `removeEventListener` below runs after the page is destroyed. But
+    // before that can happen, we end up here because of a spurious (not
+    // meant for us) "popstate".
+    //
+    // Such a spurious event might not have any state, or might not have
+    // state of the form that we expect. So only proceed if we recognize the
+    // shape of the state.
+    if (!state) return;
+    if (typeof state.quoteIndex !== "number") return;
+
+    showQuoteAtIndex(ensureNumber(state.quoteIndex));
+};
 
 const loadInitialQuote = () => {
     const quoteIndex = randomInt(parsedQuotes.quotes.length);
@@ -32,8 +51,8 @@ const loadInitialQuote = () => {
 const showQuoteAtIndex = (i: number) => {
     _quoteIndex = i;
     const quote = parsedQuotes.quotes[i];
+    console.log("showQuoteAtIndex", { i, quote });
     const output = document.getElementsByTagName("output")[0];
-    console.log(parsedQuotes)
     output.innerText = quote.toString();
 };
 
@@ -52,24 +71,4 @@ export const traverse = (word: string) => {
     showQuoteAtIndex(quoteIndex);
 };
 
-const handlePopState = (event: PopStateEvent) => {
-    const { state } = event;
-
-    // When the user presses back from this page itself the
-    // `removeEventListener` below runs after the page is destroyed. But
-    // before that can happen, we end up here because of a spurious (not
-    // meant for us) "popstate".
-    //
-    // Such a spurious event might not have any state, or might not have
-    // state of the form that we expect. So only proceed if we recognize the
-    // shape of the state.
-    if (!state) return;
-    if (typeof state.quoteIndex !== "number") return;
-
-    showQuoteAtIndex(ensureNumber(state.quoteIndex));
-};
-
-export const init = () => {
-    window.addEventListener("popstate", handlePopState);
-    loadInitialQuote();
-};
+init();
