f9936513b4071b9193b97784942305c12e7f5d77
Manav Rathi <manav@mrmr.io>
2024-10-02 22:55:54 +0530

    Simplify

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,14 @@
 import { defineConfig } from "vite";
 import { globSync } from "tinyglobby";
-import { mrmr } from "./vite-plugin-mrmr.ts";
 
-// const entryPoints = globSync(["**/*.html", "!dist", "!node_modules"]);
-const entryPoints = globSync(["index.html"]);
-
-const mdEntryPoints = ["hunger.md"];
+const entryPoints = globSync(["**/*.html", "!dist", "!node_modules"]);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [mrmr()],
   appType: "mpa" /* disable the SPA 404 fallback in dev mode. */,
   build: {
     rollupOptions: {
-      input: Object.fromEntries(
-        entryPoints
-          .map((h) => [h, h])
-          .concat(mdEntryPoints.map((m) => [m, m.replace(".md", ".html")])),
-      ),
+      input: Object.fromEntries(entryPoints.map((h) => [h, h])),
     },
   },
 });
