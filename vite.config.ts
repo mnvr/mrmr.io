817a14f00360e449404c646075a1c6a9eb737f47
Manav Rathi <manav@mrmr.io>
2024-10-06 17:32:30 +0530

    Dedup non-essential head tags

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,36 @@
-import { defineConfig } from "vite";
+import { type Plugin, defineConfig } from "vite";
 import { globSync } from "tinyglobby";
 
 const entryPoints = globSync(["**/*.html", "!dist", "!node_modules"]);
 
+const head = (): Plugin => {
+  return {
+    name: "vite-plugin-head",
+    transformIndexHtml(_, ctx) {
+      const preview =
+        ctx.originalUrl == "/" ? "/preview/blue.png" : "/preview.png";
+      return [
+        // <meta name="og:image" content="/preview/blue.png">
+        {
+          tag: "meta",
+          attrs: { name: "og:image", content: preview },
+          injectTo: "head",
+        },
+        // <link rel="icon" href="/icon.png" type="image/png">
+        {
+          tag: "link",
+          attrs: { rel: "icon", href: "/icon.png", type: "image/png" },
+          injectTo: "head",
+        },
+      ];
+    },
+  };
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   appType: "mpa" /* disable the SPA 404 fallback in dev mode. */,
+  plugins: [head()],
   build: {
     rollupOptions: {
       input: Object.fromEntries(entryPoints.map((h) => [h, h])),
