17d7fb3f2b93ee9f3d2e364dbfcceb6fc95d81e3
Manav Rathi <manav@mrmr.io>
2024-10-06 19:41:56 +0530

    Make it work with rollup too
    Otherwise rollup first transforms the CSS

--- vite.config.ts
+++ vite.config.ts
@@ -8,28 +8,31 @@ const head = async (): Promise<Plugin> => {
   const paperCss = `<style>${await readFile("paper.css", "utf-8")}</style>`;
   return {
     name: "vite-plugin-head",
-    transformIndexHtml(html, ctx) {
-      const preview =
-        ctx.originalUrl == "/" ? "/preview/blue.png" : "/preview.png";
-      html = html.replace(
-        `<link rel="stylesheet" href="./paper.css">`,
-        paperCss,
-      );
-      const tags: HtmlTagDescriptor[] = [
-        // <meta name="og:image" content="/preview/blue.png">
-        {
-          tag: "meta",
-          attrs: { name: "og:image", content: preview },
-          injectTo: "head",
-        },
-        // <link rel="icon" href="/icon.png" type="image/png">
-        {
-          tag: "link",
-          attrs: { rel: "icon", href: "/icon.png", type: "image/png" },
-          injectTo: "head",
-        },
-      ];
-      return { html, tags };
+    transformIndexHtml: {
+      order: "pre",
+      handler(html, ctx) {
+        html = html.replace(
+          `<link rel="stylesheet" href="./paper.css">`,
+          paperCss,
+        );
+        const preview =
+          ctx.originalUrl == "/" ? "/preview/blue.png" : "/preview.png";
+        const tags: HtmlTagDescriptor[] = [
+          // <meta name="og:image" content="/preview/blue.png">
+          {
+            tag: "meta",
+            attrs: { name: "og:image", content: preview },
+            injectTo: "head",
+          },
+          // <link rel="icon" href="/icon.png" type="image/png">
+          {
+            tag: "link",
+            attrs: { rel: "icon", href: "/icon.png", type: "image/png" },
+            injectTo: "head",
+          },
+        ];
+        return { html, tags };
+      },
     },
   };
 };
